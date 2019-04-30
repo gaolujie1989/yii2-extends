35af36e81813935bea794da31ad2c821b5ad47aa
Lujie <gao_lujie@live.cn>
2019-04-30 14:27:53 +0800

    add extensions.php

---
+++ extension.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+return [
+    'lujie/yii2-data-loader' => [
+        'name' => 'lujie/yii2-data-loader',
+        'version' => 'dev-master',
+        'alias' => [
+            '@lujie/data/loader' => __DIR__ . '/src',
+        ]
+    ],
+];

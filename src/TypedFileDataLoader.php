d9b1f8c4855725a3790dbbba7a13e8c47677da44
Lujie <gao_lujie@live.cn>
2019-04-30 16:04:54 +0800

    add typed file data loader

---
+++ src/TypedFileDataLoader.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\data\loader;
+
+/**
+ * Class TypedFileDataLoader
+ * @package lujie\data\loader
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class TypedFileDataLoader extends FileDataLoader
+{
+    /**
+     * @var string
+     */
+    public $typedFilePathTemplate = '{filePool}/*/config/{type}.php';
+
+    /**
+     * @param int|string $key
+     * @return array|mixed|void|null
+     * @inheritdoc
+     */
+    public function get($key)
+    {
+        $this->filePathTemplate = strtr($this->typedFilePathTemplate, ['{type}' => $key]);
+        return parent::all();
+    }
+
+    /**
+     * @return array|void
+     * @inheritdoc
+     */
+    public function all()
+    {
+        $this->filePathTemplate = strtr($this->typedFilePathTemplate, ['{type}' => '*']);
+        return parent::all();
+    }
+}

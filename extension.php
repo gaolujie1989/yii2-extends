0e0c00969ee7b0d336dd64bd77648318efc48848
Lujie <gao_lujie@live.cn>
2019-05-09 15:04:55 +0800

    fix

---
+++ extension.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+return [
+    'lujie/yii2-queuing-monitor' => [
+        'name' => 'lujie/yii2-queuing-monitor',
+        'version' => 'dev-master',
+        'alias' => [
+            '@lujie/queuing/monitor' => __DIR__ . '/src',
+        ]
+    ],
+];

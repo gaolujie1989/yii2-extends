31527fa240bed912041f25f58931a8334763f43a
Lujie <gao_lujie@live.cn>
2019-05-09 15:05:02 +0800

    fix

---
+++ extension.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+return [
+    'lujie/yii2-stock-manager' => [
+        'name' => 'lujie/yii2-stock-manager',
+        'version' => 'dev-master',
+        'alias' => [
+            '@lujie/stock' => __DIR__ . '/src',
+        ]
+    ],
+];

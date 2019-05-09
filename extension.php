19d120c7f700e9aa02320150fc16ec705bc5070f
Lujie <gao_lujie@live.cn>
2019-05-09 14:57:33 +0800

    fix

---
+++ extension.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+return [
+    'lujie/yii2-relationsave' => [
+        'name' => 'lujie/yii2-relationsave',
+        'version' => 'dev-master',
+        'alias' => [
+            '@lujie/relationsave' => __DIR__ . '/src',
+        ]
+    ],
+];

ec187f794cc481a219d5b432ff60d55de6f79404
Lujie <gao_lujie@live.cn>
2019-05-09 14:59:30 +0800

    fix

---
+++ extension.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+return [
+    'lujie/yii2-statemachine' => [
+        'name' => 'lujie/yii2-statemachine',
+        'version' => 'dev-master',
+        'alias' => [
+            '@lujie/statemachine' => __DIR__ . '/src',
+        ]
+    ],
+];

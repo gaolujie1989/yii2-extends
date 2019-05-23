a41b1fab363ef1d360a4ab071f86b3c19e229862
Lujie <gao_lujie@live.cn>
2019-05-23 15:39:17 +0800

    fix tests

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,2 +1,10 @@
 <?php
-// Here you can initialize variables that will be available to your tests
+defined('YII_DEBUG') or define('YII_DEBUG', true);
+defined('YII_ENV') or define('YII_ENV', 'test');
+defined('YII_APP_BASE_PATH') or define('YII_APP_BASE_PATH', __DIR__ . '/../../../apps');
+
+require_once YII_APP_BASE_PATH . '/vendor/autoload.php';
+require_once YII_APP_BASE_PATH . '/vendor/yiisoft/yii2/Yii.php';
+require_once YII_APP_BASE_PATH . '/common/config/bootstrap.php';
+
+Yii::setAlias('@tests', __DIR__);

187591e6e6aeb1067d3bc31c9856b42dfb31ac0f
Lujie <gao_lujie@live.cn>
2019-06-17 09:09:49 +0800

    fix

--- src/migrations/m190506_095550_upload_saved_file.php
+++ src/migrations/m190506_095550_upload_saved_file.php
@@ -13,14 +13,14 @@ class m190506_095550_upload_saved_file extends Migration
     public function safeUp()
     {
         $this->createTable($this->tableName, [
-            'upload_saved_file_id' => $this->bigPrimaryKey(),
+            'upload_saved_file_id' => $this->bigPrimaryKey()->unsigned(),
             'model_type' => $this->string(50)->notNull()->defaultValue(''),
-            'model_id' => $this->bigInteger()->notNull()->defaultValue(0),
-            'position' => $this->smallInteger()->notNull()->defaultValue(0),
+            'model_id' => $this->bigInteger()->unsigned()->notNull()->defaultValue(0),
+            'position' => $this->smallInteger()->unsigned()->notNull()->defaultValue(0),
             'file' => $this->string()->notNull(),
             'name' => $this->string()->notNull()->defaultValue(''),
             'ext' => $this->string(10)->notNull()->defaultValue(''),
-            'size' =>  $this->integer()->notNull()->defaultValue(0),
+            'size' =>  $this->integer()->unsigned()->notNull()->defaultValue(0),
         ]);
 
         $this->createIndex('idx_model_type_model_id', $this->tableName, ['model_type', 'model_id']);

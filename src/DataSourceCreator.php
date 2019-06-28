64cd4ba97acdfa22acae19549c0ba757812c9b96
Lujie <gao_lujie@live.cn>
2019-06-28 14:18:19 +0800

    fix

--- src/DataSourceCreator.php
+++ src/DataSourceCreator.php
@@ -50,6 +50,7 @@ class DataSourceCreator extends BaseObject
             $dataSource = new DataSource($this->sourceConfig);
             $dataSource->type = $sourceType;
             $dataSource->data_account_id = $account->data_account_id;
+            $dataSource->name = $sourceType . '_' . $account->name;
             $dataSource->save(false);
         }
     }

74764fa93fcb39441d05e7e706456f937a75f974
Lujie <gao_lujie@live.cn>
2019-07-01 10:55:00 +0800

    fix

---
+++ src/CombinedExchangeLoader.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\data\staging;
+
+use lujie\data\loader\DataLoaderInterface;
+use lujie\data\staging\models\DataSource;
+use yii\base\BaseObject;
+use yii\base\InvalidConfigException;
+use yii\base\NotSupportedException;
+use yii\di\Instance;
+
+/**
+ * Class CombinedExchangeLoader
+ * @package lujie\data\staging
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class CombinedExchangeLoader extends BaseObject implements DataLoaderInterface
+{
+    public $stagingExchangeLoaders = [];
+
+    /**
+     * @param int|string $key
+     * @return mixed|null
+     * @throws InvalidConfigException
+     * @inheritdoc
+     */
+    public function get($key)
+    {
+        $dataSource = DataSource::findOne($key);
+        if ($dataSource === null) {
+            return null;
+        }
+
+        $exchangeLoader = $this->getStagingExchangeLoader($dataSource->dataAccount->type);
+        return $exchangeLoader->get($key);
+    }
+
+    /**
+     * @param string $accountType
+     * @return DataLoaderInterface
+     * @throws InvalidConfigException
+     * @inheritdoc
+     */
+    protected function getStagingExchangeLoader(string $accountType): DataLoaderInterface
+    {
+        if (empty($this->stagingExchangeLoaders[$accountType])) {
+            throw new InvalidConfigException('ExchangeLoader not config');
+        }
+
+        $this->stagingExchangeLoaders[$accountType] = Instance::ensure($this->stagingExchangeLoaders[$accountType], DataLoaderInterface::class);
+        return $this->stagingExchangeLoaders[$accountType];
+    }
+
+    /**
+     * @return array|null
+     * @throws NotSupportedException
+     * @inheritdoc
+     */
+    public function all(): ?array
+    {
+        throw new NotSupportedException('not support');
+    }
+}

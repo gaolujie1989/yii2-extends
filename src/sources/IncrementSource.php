fa0a9a03a97e0f9e392f3d36b4f32518cf0e2138
Lujie <gao_lujie@live.cn>
2019-07-02 10:30:33 +0800

    fix

--- src/sources/IncrementSource.php
+++ src/sources/IncrementSource.php
@@ -57,7 +57,7 @@ abstract class IncrementSource extends BaseObject implements BatchSourceInterfac
         if (empty($this->sourceKey)) {
             throw new InvalidConfigException('The "sourceKey" property must be set');
         }
-        $this->incrementCondition = $this->dataStorage->get($this->sourceKey);
+        $this->incrementCondition = $this->dataStorage->get($this->sourceKey) ?: [];
     }
 
     /**

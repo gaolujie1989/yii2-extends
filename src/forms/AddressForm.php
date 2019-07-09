6a64aa4fcdb6613d5e004d2ecd6720531ba2d02d
Lujie <gao_lujie@live.cn>
2019-07-09 15:33:07 +0800

    fix

---
+++ src/forms/AddressForm.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\common\address\forms;
+
+use lujie\common\address\models\Address;
+
+/**
+ * Class AddressForm
+ * @package lujie\sales\order\center\forms
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class AddressForm extends Address
+{
+    /**
+     * if identify by signature,
+     * no update for exists address, create address instead
+     * if signature is same, not create address, return exist address instead
+     * @var bool
+     */
+    public $identifyBySignature = true;
+
+    /**
+     * @param bool $runValidation
+     * @param null $attributeNames
+     * @return bool
+     * @inheritdoc
+     */
+    public function save($runValidation = true, $attributeNames = null): bool
+    {
+        if ($this->identifyBySignature) {
+            $signature = $this->generateSignature();
+            $address = static::findBySignature($signature);
+            if ($address === null) {
+                $this->setIsNewRecord(true);
+                return parent::save();
+            }
+            $this->refreshInternal($address);
+            return true;
+        }
+        return parent::save();
+    }
+}

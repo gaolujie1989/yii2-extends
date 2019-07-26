bb786f7e2f2f98bb85956e3a65d8de5976773877
Lujie <gao_lujie@live.cn>
2019-07-26 14:13:52 +0800

    fix

--- src/PlentyMarketsRestClient.php
+++ src/PlentyMarketsRestClient.php
@@ -9,6 +9,7 @@ use lujie\extend\authclient\RestOAuth2Client;
 use yii\authclient\InvalidResponseException;
 use yii\authclient\OAuthToken;
 use yii\helpers\Inflector;
+use yii\helpers\Json;
 
 /**
  * Class PlentyMarketsRestClient
@@ -314,6 +315,7 @@ use yii\helpers\Inflector;
  * @method array updateListingMarketTexts($data)
  * @method array deleteListingMarketTexts($data)
  *
+ * @method array batchRequest($data)
  * @method array getOrderPackageNumbers($data)
  *
  * @package lujie\plentyMarkets
@@ -435,6 +437,7 @@ class PlentyMarketsRestClient extends RestOAuth2Client
     ];
 
     public $apiMethods = [
+        'batchRequest' => ['POST', 'batch'],
         'searchItemVariations' => ['GET', 'items/variations'],
         'listVariations' => ['GET', 'items/variations'],
 
@@ -563,7 +566,7 @@ class PlentyMarketsRestClient extends RestOAuth2Client
      * @throws \yii\web\NotFoundHttpException
      * @inheritdoc
      */
-    public function batch(string $resource, array $condition = [],  int $batchSize = 100): \Iterator
+    public function batch(string $resource, array $condition = [], int $batchSize = 100): \Iterator
     {
         $condition['itemsPerPage'] = $batchSize;
         $listMethod = 'list' . Inflector::pluralize($resource);
@@ -598,4 +601,71 @@ class PlentyMarketsRestClient extends RestOAuth2Client
         }
     }
 
+    /**
+     * @param array $variationIds
+     * @return array
+     * @inheritdoc
+     */
+    public function getWarehouseStocksByVariationIds(array $variationIds): array
+    {
+        $payloads = array_map(static function ($variationId) {
+            return [
+                'resource' => 'rest/stockmanagement/stock?variationId=' . $variationId,
+                'method' => 'GET',
+            ];
+        }, $variationIds);
+        $batchResponse = $this->batchRequest(['payloads' => $payloads]);
+        $warehouseStocks = [];
+        foreach ($batchResponse as $response) {
+            $content = Json::decode($response['content']);
+            $warehouseStocks[] = $content['entries'];
+        }
+        return array_merge(...$warehouseStocks);
+    }
+
+    /**
+     * @param array $orderIds
+     * @return array
+     * @inheritdoc
+     */
+    public function getOrdersByOrderIds(array $orderIds): array
+    {
+        $payloads = array_map(static function ($orderId) {
+            return [
+                'resource' => "rest/orders/{$orderId}",
+                'method' => 'GET',
+            ];
+        }, $orderIds);
+        $batchResponse = $this->batchRequest(['payloads' => $payloads]);
+        $orders = [];
+        foreach ($batchResponse as $response) {
+            $orderId = substr($response['resource'], 12);
+            $content = Json::decode($response['content']);
+            $orders[$orderId] = $content;
+        }
+        return $orders;
+    }
+
+    /**
+     * @param array $orderIds
+     * @return array
+     * @inheritdoc
+     */
+    public function getOrderPackageNumbersByOrderIds(array $orderIds): array
+    {
+        $payloads = array_map(static function ($orderId) {
+            return [
+                'resource' => "rest/orders/{$orderId}/packagenumbers",
+                'method' => 'GET',
+            ];
+        }, $orderIds);
+        $batchResponse = $this->batchRequest(['payloads' => $payloads]);
+        $orderPackageNumbers = [];
+        foreach ($batchResponse as $response) {
+            $orderId = substr($response['resource'], 12, -15);
+            $content = Json::decode($response['content']);
+            $orderPackageNumbers[$orderId] = $content;
+        }
+        return $orderPackageNumbers;
+    }
 }

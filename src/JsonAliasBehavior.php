f7ec7513eedded9f02dee29ef0157e2e35307d60
Lujie <gao_lujie@live.cn>
2019-07-26 09:05:39 +0800

    fix

---
+++ src/JsonAliasBehavior.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * @copyright Copyright (c) 2017
+ */
+
+namespace lujie\alias\behaviors;
+
+
+use yii\helpers\Json;
+
+/**
+ * Class TimestampAliasBehavior
+ * @package lujie\core\behaviors
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class JsonAliasBehavior extends AliasPropertyBehavior
+{
+    /**
+     * @param $name
+     * @return int|mixed|string
+     * @throws \Exception
+     * @inheritdoc
+     */
+    public function getAliasProperty($name)
+    {
+        $value = parent::getAliasProperty($name);
+        return is_string($value) ? $value : Json::encode($name);
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     * @throws \Exception
+     * @inheritdoc
+     */
+    public function setAliasProperty($name, $value): void
+    {
+        $value = is_array($value) ? $value : Json::decode($value);
+        parent::setAliasProperty($name, $value);
+    }
+}

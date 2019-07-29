87c7a4e681646ea96459d77c8ae409d04db2b059
Lujie <gao_lujie@live.cn>
2019-07-29 17:24:08 +0800

    fix

--- src/CronTask.php
+++ src/CronTask.php
@@ -5,17 +5,45 @@
 
 namespace lujie\scheduling;
 
+use lujie\alias\behaviors\JsonAliasBehavior;
 use lujie\executing\ExecutableTrait;
 use lujie\executing\LockableTrait;
 use lujie\executing\QueueableTrait;
-use yii\base\BaseObject;
+use yii\base\Model;
 
 /**
  * Class Task
  * @package lujie\scheduling
  * @author Lujie Zhou <gao_lujie@live.cn>
  */
-class CronTask extends BaseObject implements ScheduleTaskInterface
+class CronTask extends Model implements ScheduleTaskInterface
 {
     use CronScheduleTrait, ExecutableTrait, LockableTrait, QueueableTrait;
+
+    /**
+     * @return array
+     * @inheritdoc
+     */
+    public function behaviors(): array
+    {
+        return array_merge(parent::behaviors(), [
+            'json' => [
+                'class' => JsonAliasBehavior::class,
+                'aliasProperties' => [
+                    'executableJson' => 'executable'
+                ]
+            ]
+        ]);
+    }
+
+    /**
+     * @return array
+     * @inheritdoc
+     */
+    public function fields(): array
+    {
+        return array_merge(parent::fields(), [
+            'executableJson'
+        ]);
+    }
 }

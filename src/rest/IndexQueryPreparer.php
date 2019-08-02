10271dca795ebdfb3fe24ff7a6f22095cb97b06f
Lujie <gao_lujie@live.cn>
2019-08-02 17:26:15 +0800

    fix

---
+++ src/rest/IndexQueryPreparer.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\extend\rest;
+
+use lujie\extend\helpers\ClassHelper;
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\db\BaseActiveRecord;
+use yii\db\QueryInterface;
+use yii\rest\Action;
+
+/**
+ * Class IndexDataProviderPreparer
+ * @package lujie\extend\rest
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class IndexQueryPreparer
+{
+    /**
+     * @var BaseActiveRecord
+     */
+    public $searchModelClass;
+
+    /**
+     * @var string
+     */
+    public $queryMethod = 'query';
+
+    /**
+     * @var string
+     */
+    public $formName = '';
+
+    /**
+     * @param Action $action
+     * @return QueryInterface
+     * @throws InvalidConfigException
+     * @inheritdoc
+     */
+    public function prepare(Action $action): QueryInterface
+    {
+        $requestParams = Yii::$app->getRequest()->getBodyParams();
+        if (empty($requestParams)) {
+            $requestParams = Yii::$app->getRequest()->getQueryParams();
+        }
+
+        $searchClass = $this->searchModelClass ?: ClassHelper::getSearchClass($action->modelClass);
+        if ($searchClass) {
+            $searchModel = Yii::createObject($searchClass);
+            if (method_exists($searchModel, $this->queryMethod)) {
+                $searchModel->load($requestParams, $this->formName);
+                return $searchModel->{$this->queryMethod}();
+            }
+        }
+
+        /* @var $model BaseActiveRecord */
+        $model = new $action->modelClass();
+        if (method_exists($model, $this->queryMethod)) {
+            $model->load($requestParams, $this->formName);
+            return $model->{$this->queryMethod}();
+        }
+
+        return $model::find();
+    }
+}

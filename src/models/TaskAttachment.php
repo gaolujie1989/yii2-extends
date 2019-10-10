b22eaf95184aff97149a49ea686d7c79b2289d2d
Lujie <gao_lujie@live.cn>
2019-10-10 14:03:26 +0800

    fix

--- src/models/TaskAttachment.php
+++ src/models/TaskAttachment.php
@@ -6,10 +6,15 @@
 namespace lujie\project\models;
 
 
+use lujie\alias\behaviors\AliasPropertyBehavior;
 use lujie\upload\models\UploadSavedFile;
 
 /**
  * Class TaskAttachment
+ *
+ * @property int $task_id
+ * @property int $project_id
+ *
  * @package lujie\project\models
  * @author Lujie Zhou <gao_lujie@live.cn>
  */
@@ -18,4 +23,32 @@ class TaskAttachment extends UploadSavedFile
     public const MODEL_TYPE_TASK_ATTACHMENT = 'TASK_ATTACHMENT';
 
     public const MODEL_TYPE = self::MODEL_TYPE_TASK_ATTACHMENT;
+
+    /**
+     * @return array
+     * @inheritdoc
+     */
+    public function rules(): array
+    {
+        return array_merge(parent::rules(), [
+            [['task_id', 'project_id'], 'integer']
+        ]);
+    }
+
+    /**
+     * @return array
+     * @inheritdoc
+     */
+    public function behaviors(): array
+    {
+        return array_merge(parent::behaviors(), [
+            'alias' => [
+                'class' => AliasPropertyBehavior::class,
+                'aliasProperties' => [
+                    'task_id' => 'model_id',
+                    'project_id' => 'model_parent_id',
+                ]
+            ]
+        ]);
+    }
 }

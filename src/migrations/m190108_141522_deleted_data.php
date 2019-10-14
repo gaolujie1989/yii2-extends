ba22f067bb30255947aa1bdb6a1f5fbf492909c4
Lujie <gao_lujie@live.cn>
2019-10-14 09:52:39 +0800

    remove bigint unsigned, to fix fetch data from mysql with string type not int type

--- src/migrations/m190108_141522_deleted_data.php
+++ src/migrations/m190108_141522_deleted_data.php
@@ -30,9 +30,9 @@ class m190108_141522_deleted_data extends \yii\db\Migration
     public function safeUp()
     {
         $this->createTable($this->tableName, [
-            'id' => $this->bigPrimaryKey()->unsigned(),
+            'id' => $this->bigPrimaryKey(),
             'table_name' => $this->string(50)->notNull()->defaultValue(''),
-            'row_id' => $this->bigInteger()->unsigned()->notNull()->defaultValue(0),
+            'row_id' => $this->bigInteger()->notNull()->defaultValue(0),
             'row_data' => $this->json(),
         ]);
 

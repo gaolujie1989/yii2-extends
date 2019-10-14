6435d5d7f5a210a745f700643def7a503956f860
Lujie <gao_lujie@live.cn>
2019-10-14 09:52:43 +0800

    remove bigint unsigned, to fix fetch data from mysql with string type not int type

--- tests/unit/fixtures/migrations/m190617_132514_test_snapshoot_models.php
+++ tests/unit/fixtures/migrations/m190617_132514_test_snapshoot_models.php
@@ -7,7 +7,7 @@ class m190617_132514_test_snapshoot_models extends Migration
     public function safeUp()
     {
         $this->createTable('test_item', [
-            'test_item_id' => $this->bigPrimaryKey()->unsigned(),
+            'test_item_id' => $this->bigPrimaryKey(),
             'item_no' => $this->string()->notNull()->defaultValue(''),
             'item_name' => $this->string()->notNull()->defaultValue(''),
             'status' => $this->tinyInteger()->unsigned()->notNull()->defaultValue(0),
@@ -15,13 +15,13 @@ class m190617_132514_test_snapshoot_models extends Migration
         ]);
 
         $this->createTable('test_item_snapshoot', [
-            'test_item_snapshoot_id' => $this->bigPrimaryKey()->unsigned(),
-            'test_item_id' => $this->bigInteger()->unsigned()->notNull(),
+            'test_item_snapshoot_id' => $this->bigPrimaryKey(),
+            'test_item_id' => $this->bigInteger()->notNull(),
             'item_no' => $this->string()->notNull()->defaultValue(''),
             'item_name' => $this->string()->notNull()->defaultValue(''),
             'status' => $this->tinyInteger()->unsigned()->notNull()->defaultValue(0),
             'updated_at' => $this->integer()->unsigned()->notNull()->defaultValue(0),
         ]);
-        $this->addColumn('test_item', 'snapshoot_id', $this->bigInteger()->unsigned()->notNull()->defaultValue(0)->after('test_item_id'));
+        $this->addColumn('test_item', 'snapshoot_id', $this->bigInteger()->notNull()->defaultValue(0)->after('test_item_id'));
     }
 }

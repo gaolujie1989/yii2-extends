2797e09de00bdb4a7f7d431d68fc2c66e56a6e93
Lujie <gao_lujie@live.cn>
2019-10-14 12:01:16 +0800

    fix

--- src/FulfillmentManager.php
+++ src/FulfillmentManager.php
@@ -71,7 +71,7 @@ class FulfillmentManager extends Component implements BootstrapInterface
     public function bootstrap($app): void
     {
         Event::on(FulfillmentItem::class, BaseActiveRecord::EVENT_AFTER_INSERT, [$this, 'afterFulfillmentItemSaved']);
-        Event::on(FulfillmentItem::class, BaseActiveRecord::EVENT_AFTER_UPDATE, [$this, 'afterFulfillmentItemSaved']);
+//        Event::on(FulfillmentItem::class, BaseActiveRecord::EVENT_AFTER_UPDATE, [$this, 'afterFulfillmentItemSaved']);
         Event::on(FulfillmentOrder::class, BaseActiveRecord::EVENT_AFTER_INSERT, [$this, 'afterFulfillmentOrderCreated']);
         Event::on(FulfillmentOrder::class, BaseActiveRecord::EVENT_AFTER_UPDATE, [$this, 'afterFulfillmentOrderUpdated']);
     }

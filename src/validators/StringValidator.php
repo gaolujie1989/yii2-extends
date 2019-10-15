1d43c9dc64b62afd3c76ea8c46a5b11a3423326d
Lujie <gao_lujie@live.cn>
2019-10-15 11:08:27 +0800

    fix

---
+++ src/validators/StringValidator.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\extend\validators;
+
+
+/**
+ * Class StringValidator
+ * @package lujie\extend\validators
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class StringValidator extends \yii\validators\StringValidator
+{
+    public $trim = true;
+
+    /**
+     * @param $model
+     * @param $attribute
+     * @inheritdoc
+     */
+    public function validateAttribute($model, $attribute): void
+    {
+        if ($this->trim) {
+            $model->{$attribute} = trim($model->{$attribute});
+        }
+        return parent::validateAttribute($model, $attribute);
+    }
+
+    /**
+     * @param $value
+     * @return array|null
+     * @throws \yii\base\NotSupportedException
+     * @inheritdoc
+     */
+    protected function validateValue($value): ?array
+    {
+        if ($this->trim) {
+            $value = trim($value);
+        }
+        return parent::validateValue($value);
+    }
+}

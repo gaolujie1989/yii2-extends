c434eb74865e6a8cf37c3cf1313e429bba098502
Lujie <gao_lujie@live.cn>
2019-10-18 16:51:30 +0800

    add id($id) query method

--- src/models/UploadSavedFileQuery.php
+++ src/models/UploadSavedFileQuery.php
@@ -7,6 +7,7 @@ use lujie\db\fieldQuery\behaviors\FieldQueryBehavior;
 /**
  * This is the ActiveQuery class for [[UploadedFile]].
  *
+ * @method UploadSavedFileQuery id($id)
  * @method UploadSavedFileQuery modelType($modelType)
  * @method UploadSavedFileQuery modelId($modelId)
  * @method UploadSavedFileQuery modelParentId($modelParentId)

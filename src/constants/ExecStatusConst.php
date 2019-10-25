99eeb050ff47723508ae8400c281443087bcc9c6
Lujie <gao_lujie@live.cn>
2019-10-25 15:40:50 +0800

    fix

---
+++ src/constants/ExecStatusConst.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\extend\constants;
+
+/**
+ * Class ExecStatusConst
+ * @package lujie\extend\constants
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class ExecStatusConst
+{
+    public const EXEC_STATUS_PENDING = 0;
+    public const EXEC_STATUS_RUNNING = 1;
+    public const EXEC_STATUS_SUCCESS = 10;
+    public const EXEC_STATUS_FAILED = 11;
+    public const EXEC_STATUS_SKIPPED = 13;
+    public const EXEC_STATUS_QUEUED = 12;
+}
---
+++ src/constants/StatusConst.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\extend\constants;
+
+/**
+ * Class StatusConst
+ * @package lujie\extend\constants
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class StatusConst
+{
+    public const STATUS_ACTIVE = 10;
+    public const STATUS_INACTIVE = 0;
+    public const STATUS_ENABLE = 10;
+    public const STATUS_DISABLE = 0;
+}

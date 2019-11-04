823f7b5ada58c8d233529e882fccbddb3de28e96
Lujie <gao_lujie@live.cn>
2019-11-04 14:34:51 +0800

    fix

---
+++ tests/unit/rest/IndexDataProviderPreparerTest.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\extend\tests\unit\rest;
+
+
+use lujie\extend\rest\IndexQueryPreparer;
+use lujie\extend\rest\RestResponseBehavior;
+use lujie\extend\tests\unit\fixtures\searches\MigrationSearch;
+use yii\web\Response;
+
+class IndexDataProviderPreparerTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \lujie\extend\tests\UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function testMe(): void
+    {
+    }
+}

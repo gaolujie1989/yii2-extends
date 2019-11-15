100432df959a68d1b8604c54917df9eb2844d3f7
Lujie <gao_lujie@live.cn>
2019-11-15 09:20:07 +0800

    add DbConnectionTrait

--- src/base/db/ActiveRecord.php
+++ src/base/db/ActiveRecord.php
@@ -6,13 +6,11 @@
 namespace lujie\data\recording\base\db;
 
 
+use lujie\extend\db\DbConnectionTrait;
 use lujie\extend\db\IdFieldTrait;
 use lujie\extend\db\SaveTrait;
 use lujie\extend\db\TraceableBehaviorTrait;
 use lujie\extend\db\TransactionTrait;
-use Yii;
-use yii\base\InvalidConfigException;
-use yii\db\Connection;
 
 /**
  * Class ActiveRecord
@@ -21,17 +19,5 @@ use yii\db\Connection;
  */
 class ActiveRecord extends \yii\db\ActiveRecord
 {
-    use TraceableBehaviorTrait, IdFieldTrait, SaveTrait, TransactionTrait;
-
-    /**
-     * @return Connection|object
-     * @throws InvalidConfigException
-     * @inheritdoc
-     */
-    public static function getDb(): Connection
-    {
-        $app = Yii::$app;
-        $db = Yii::$app->params['recordingDB'] ?? null;
-        return $db ? $app->get($db) : parent::getDb();
-    }
+    use TraceableBehaviorTrait, IdFieldTrait, SaveTrait, TransactionTrait, DbConnectionTrait;
 }

e91f6e75e5b7cca596e3333b3f6ba98b2e21e125
Lujie <gao_lujie@live.cn>
2019-11-15 09:08:36 +0800

    fix

---
+++ src/db/DbConnectionTrait.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\extend\db;
+
+use Yii;
+use yii\base\InvalidConfigException;
+use yii\db\Connection;
+
+/**
+ * Trait IdFieldsTrait
+ * @package lujie\extend\db
+ */
+trait DbConnectionTrait
+{
+    /**
+     * @return Connection|object
+     * @throws InvalidConfigException
+     * @inheritdoc
+     */
+    public static function getDb(): Connection
+    {
+        $app = Yii::$app;
+        $db = $app->params['modelDBs'][self::class] ?? 'db';
+        return $app->get($db);
+    }
+}

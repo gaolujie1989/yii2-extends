ac522a9a922c50be09a39add9983bd08f12ad36a
Lujie <gao_lujie@live.cn>
2019-12-16 18:01:31 +0800

    fix

--- tests/unit/db/ActiveRecordTracerTest.php
+++ tests/unit/db/ActiveRecordTracerTest.php
@@ -5,8 +5,10 @@
 
 namespace lujie\extend\test\unit\db;
 
+use lujie\ar\relation\behaviors\tests\unit\fixtures\models\TestOrder;
 use lujie\extend\db\ActiveRecordTracer;
 use lujie\extend\tests\unit\mocks\MockActiveRecord;
+use Yii;
 
 class ActiveRecordTracerTest extends \Codeception\Test\Unit
 {
@@ -27,7 +29,7 @@ class ActiveRecordTracerTest extends \Codeception\Test\Unit
      * @throws \yii\base\InvalidConfigException
      * @inheritdoc
      */
-    public function testMe(): void
+    public function tes1tMe(): void
     {
         $activeRecordTracer = new ActiveRecordTracer();
         $activeRecordTracer->bootstrap(null);
@@ -52,5 +54,57 @@ class ActiveRecordTracerTest extends \Codeception\Test\Unit
         ];
         $attributes = $mockActiveRecord->getAttributes(['created_by', 'created_at', 'updated_by', 'updated_at']);
         $this->assertEquals($expected, $attributes);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function testProfile(): void
+    {
+        $count = 100;
+        $startAt = microtime(true);
+        for ($i = 0; $i < $count; $i++) {
+            $testOrder = new TestOrder();
+            $testOrder->order_no = 'orderNo' . $i;
+            $testOrder->customer_email = 'xxx' . $i . '@xxx.com';
+            $testOrder->shipping_address_id = $i;
+            $testOrder->order_amount = $i;
+            $testOrder->paid_amount = $i;
+            $testOrder->status = 0;
+            $testOrder->save(false);
+        }
+        $endAt = microtime(true);
+        $createSpendTime = $endAt - $startAt;
+
+        $startAt = microtime(true);
+        $testOrders = TestOrder::find()->all();
+        $endAt = microtime(true);
+        $querySpendTime = $endAt - $startAt;
+
+        TestOrder::getDb()->createCommand()->truncateTable(TestOrder::tableName());
+        $activeRecordTracer = new ActiveRecordTracer();
+        Yii::$app->set('activeRecordTracer', $activeRecordTracer);
+        Yii::$app->get('activeRecordTracer');
+        $activeRecordTracer->bootstrap(Yii::$app);
+        $startAt = microtime(true);
+        for ($i = 0; $i < $count; $i++) {
+            $testOrder = new TestOrder();
+            $testOrder->order_no = 'orderNo' . $i;
+            $testOrder->customer_email = 'xxx' . $i . '@xxx.com';
+            $testOrder->shipping_address_id = $i;
+            $testOrder->order_amount = $i;
+            $testOrder->paid_amount = $i;
+            $testOrder->status = 0;
+            $testOrder->save(false);
+        }
+        $endAt = microtime(true);
+        $createSpendTime2 = $endAt - $startAt;
+
+        $startAt = microtime(true);
+        $testOrders = TestOrder::find()->all();
+        $endAt = microtime(true);
+        $querySpendTime2 = $endAt - $startAt;
+
+        $this->assertTrue(false, "$createSpendTime2 < $createSpendTime, $querySpendTime2 < $querySpendTime");
     }
 }

e7832881788092456569b6fbb171937ef919cfeb
Lujie <gao_lujie@live.cn>
2020-02-28 15:34:38 +0800

    add gii template

---
+++ src/gii/generators/model/extend/model.php
@@ -0,0 +1,117 @@
+<?php
+/**
+ * This is the template for generating the model class of a specified table.
+ */
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\model\Generator */
+/* @var $tableName string full table name */
+/* @var $className string class name */
+/* @var $queryClassName string query class name */
+/* @var $tableSchema yii\db\TableSchema */
+/* @var $properties array list of properties (property => [type, name. comment]) */
+/* @var $labels string[] list of attribute labels (name => label) */
+/* @var $rules string[] list of validation rules */
+/* @var $relations array list of relations (name => relation declaration) */
+
+unset(
+    $properties['created_at'],
+    $properties['created_by'],
+    $properties['updated_at'],
+    $properties['updated_by'],
+    $labels['created_at'],
+    $labels['created_by'],
+    $labels['updated_at'],
+    $labels['updated_by']
+);
+
+echo "<?php\n";
+?>
+
+namespace <?= $generator->ns ?>;
+
+use lujie\extend\db\IdFieldTrait;
+use lujie\extend\db\SaveTrait;
+use lujie\extend\db\TraceableBehaviorTrait;
+use lujie\extend\db\TransactionTrait;
+use Yii;
+
+/**
+* This is the model class for table "<?= $generator->generateTableName($tableName) ?>".
+*
+<?php foreach ($properties as $property => $data): ?>
+  * @property <?= "{$data['type']} \${$property}" . ($data['comment'] ? ' ' . strtr($data['comment'], ["\n" => ' ']) : '') . "\n" ?>
+<?php endforeach; ?>
+<?php if (!empty($relations)): ?>
+  *
+    <?php foreach ($relations as $name => $relation): ?>
+    * @property <?= $relation[1] . ($relation[2] ? '[]' : '') . ' $' . lcfirst($name) . "\n" ?>
+    <?php endforeach; ?>
+<?php endif; ?>
+*/
+class <?= $className ?> extends <?= '\\' . ltrim($generator->baseClass, '\\') . "\n" ?>
+{
+use TraceableBehaviorTrait, IdFieldTrait, SaveTrait, TransactionTrait;
+
+/**
+* {@inheritdoc}
+*/
+public static function tableName(): string
+{
+return '<?= $generator->generateTableName($tableName) ?>';
+}
+<?php if ($generator->db !== 'db'): ?>
+
+  /**
+  * @return \yii\db\Connection the database connection used by this AR class.
+  */
+  public static function getDb(): array
+  {
+  return Yii::$app->get('<?= $generator->db ?>');
+  }
+<?php endif; ?>
+
+/**
+* {@inheritdoc}
+*/
+public function rules(): array
+{
+return [<?= empty($rules) ? '' : ("\n            " . implode(",\n            ", $rules) . ",\n        ") ?>];
+}
+
+/**
+* {@inheritdoc}
+*/
+public function attributeLabels(): array
+{
+return [
+<?php foreach ($labels as $name => $label): ?>
+    <?= "'$name' => " . $generator->generateString($label) . ",\n" ?>
+<?php endforeach; ?>
+];
+}
+<?php foreach ($relations as $name => $relation): ?>
+
+  /**
+  * @return \yii\db\ActiveQuery
+  */
+  public function get<?= $name ?>(): <?= $queryClassName . 'Query' ?>
+  {
+    <?= $relation[0] . "\n" ?>
+  }
+<?php endforeach; ?>
+<?php if ($queryClassName): ?>
+    <?php
+    $queryClassFullName = ($generator->ns === $generator->queryNs) ? $queryClassName : '\\' . $generator->queryNs . '\\' . $queryClassName;
+    echo "\n";
+    ?>
+  /**
+  * {@inheritdoc}
+  * @return <?= $queryClassFullName ?> the active query used by this AR class.
+  */
+  public static function find(): <?= $queryClassFullName . "\n" ?>
+  {
+  return new <?= $queryClassFullName ?>(static::class);
+  }
+<?php endif; ?>
+}
---
+++ src/gii/generators/model/extend/query.php
@@ -0,0 +1,79 @@
+<?php
+/**
+ * This is the template for generating the ActiveQuery class.
+ */
+
+/* @var $this yii\web\View */
+/* @var $generator yii\gii\generators\model\Generator */
+/* @var $tableName string full table name */
+/* @var $className string class name */
+/* @var $tableSchema yii\db\TableSchema */
+/* @var $labels string[] list of attribute labels (name => label) */
+/* @var $rules string[] list of validation rules */
+/* @var $relations array list of relations (name => relation declaration) */
+/* @var $className string class name */
+
+/* @var $modelClassName string related model class name */
+
+use yii\helpers\Inflector;
+
+$modelFullClassName = $modelClassName;
+if ($generator->ns !== $generator->queryNs) {
+    $modelFullClassName = '\\' . $generator->ns . '\\' . $modelFullClassName;
+}
+
+$queryFields = [];
+foreach ($labels as $name => $label) {
+    if (in_array(substr($name, -3), ['_id', '_no'], true)) {
+        $queryFields[lcfirst(Inflector::camelize($name))] = $name;
+    }
+}
+
+echo "<?php\n";
+?>
+
+namespace <?= $generator->queryNs ?>;
+
+use lujie\db\fieldQuery\behaviors\FieldQueryBehavior;
+
+/**
+* This is the ActiveQuery class for [[<?= $modelFullClassName ?>]].
+*
+* @method <?= $className ?> id($id)
+* @method <?= $className ?> orderById($sort = SORT_ASC)
+* @method int getId()
+* @method array getIds()
+*
+<?php foreach ($queryFields as $name => $field) {
+    echo " * @method $className $name(\$$name)\n";
+} ?>
+*
+* @method array|<?= $modelFullClassName ?>[] all($db = null)
+* @method array|<?= $modelFullClassName ?>|null one($db = null)
+* @method array|<?= $modelFullClassName ?>[][] batch($db = null)
+* @method array|<?= $modelFullClassName ?>[] each($db = null)
+*
+* @see <?= $modelFullClassName . "\n" ?>
+*/
+class <?= $className ?> extends <?= '\\' . ltrim($generator->queryBaseClass, '\\') . "\n" ?>
+{
+
+/**
+* @return array
+* @inheritdoc
+*/
+public function behaviors(): array
+{
+return [
+'fieldQuery' => [
+'class' => FieldQueryBehavior::class,
+'queryFields' => [
+<?php foreach ($queryFields as $name => $field) {
+    echo "                    '{$name}' => '{$field}',\n";
+} ?>
+]
+]
+];
+}
+
+}

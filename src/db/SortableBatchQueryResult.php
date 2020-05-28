4a8415249b725fb255d30ba5c3617035749ef8a4
Lujie <gao_lujie@live.cn>
2020-05-28 09:28:57 +0800

    fix

--- src/db/SortableBatchQueryResult.php
+++ src/db/SortableBatchQueryResult.php
@@ -52,7 +52,6 @@ class SortableBatchQueryResult extends \yii\db\BatchQueryResult
                 }
             }
         } else if ($this->query instanceof ActiveQuery) {
-
             /** @var BaseActiveRecord $modelClass */
             $modelClass = $this->query->modelClass;
             $column = $modelClass::primaryKey()[0];

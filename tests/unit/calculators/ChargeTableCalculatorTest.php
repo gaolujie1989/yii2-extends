70c9c3fed08fda07da674bd93de8b74d664c19a8
Lujie <gao_lujie@live.cn>
2020-07-14 18:35:54 +0800

    fix

--- tests/unit/calculators/ChargeTableCalculatorTest.php
+++ tests/unit/calculators/ChargeTableCalculatorTest.php
@@ -45,7 +45,7 @@ class ChargeTableCalculatorTest extends \Codeception\Test\Unit
     {
         $chargeableItem1 = new ChargeableItem();
         $chargeableItem1->customType = 'MOCK_CHARGE_C1';
-        $chargeableItem1->limitValue = 11000;
+        $chargeableItem1->limitValue = 1100;
         $chargeableItem1->chargedAt = time();
         $chargeableItem1->additional = [
             'qty' => 1,
@@ -53,9 +53,33 @@ class ChargeTableCalculatorTest extends \Codeception\Test\Unit
         ];
         $chargeableItem2 = new ChargeableItem();
         $chargeableItem2->customType = 'MOCK_CHARGE_C1';
-        $chargeableItem2->limitValue = 29000;
+        $chargeableItem2->limitValue = 2200;
         $chargeableItem2->chargedAt = time();
         $chargeableItem2->additional = [
+            'qty' => 2,
+            'owner_id' => 1,
+        ];
+        $chargeableItem22 = new ChargeableItem();
+        $chargeableItem22->customType = 'MOCK_CHARGE_C1';
+        $chargeableItem22->limitValue = 5500;
+        $chargeableItem22->chargedAt = time();
+        $chargeableItem22->additional = [
+            'qty' => 1,
+            'owner_id' => 1,
+        ];
+        $chargeableItem3 = new ChargeableItem();
+        $chargeableItem3->customType = 'MOCK_CHARGE_C1';
+        $chargeableItem3->limitValue = 12500;
+        $chargeableItem3->chargedAt = time();
+        $chargeableItem3->additional = [
+            'qty' => 1,
+            'owner_id' => 1,
+        ];
+        $chargeableItem4 = new ChargeableItem();
+        $chargeableItem4->customType = 'MOCK_CHARGE_C1';
+        $chargeableItem4->limitValue = 22500;
+        $chargeableItem4->chargedAt = time();
+        $chargeableItem4->additional = [
             'qty' => 2,
             'owner_id' => 1,
         ];
@@ -65,6 +89,9 @@ class ChargeTableCalculatorTest extends \Codeception\Test\Unit
                 'data' => [
                     '1' => $chargeableItem1,
                     '2' => $chargeableItem2,
+                    '22' => $chargeableItem22,
+                    '3' => $chargeableItem3,
+                    '4' => $chargeableItem4,
                 ]
             ]
         ]);
@@ -78,8 +105,9 @@ class ChargeTableCalculatorTest extends \Codeception\Test\Unit
         $this->assertEquals(1, $chargePrice->price_table_id);
         $this->assertEquals(1, $chargePrice->qty);
         $this->assertEquals(120, $chargePrice->price_cent);
-        $this->assertEquals(12, $chargePrice->discount_cent);
-        $this->assertEquals(108, $chargePrice->grand_total_cent);
+        $this->assertEquals(120, $chargePrice->subtotal_cent);
+        $this->assertEquals(10, $chargePrice->discount_cent);
+        $this->assertEquals(110, $chargePrice->grand_total_cent);
         $this->assertEquals('EUR', $chargePrice->currency);
 
         $testOrder = new TestOrder(['test_order_id' => 2]);
@@ -91,9 +119,55 @@ class ChargeTableCalculatorTest extends \Codeception\Test\Unit
         $chargePrice->save(false);
         $this->assertEquals(2, $chargePrice->price_table_id);
         $this->assertEquals(2, $chargePrice->qty);
-        $this->assertEquals(390, $chargePrice->price_cent);
-        $this->assertEquals(94, $chargePrice->discount_cent);
-        $this->assertEquals(686, $chargePrice->grand_total_cent);
+        $this->assertEquals(240, $chargePrice->price_cent);
+        $this->assertEquals(480, $chargePrice->subtotal_cent);
+        $this->assertEquals(96, $chargePrice->discount_cent);
+        $this->assertEquals(384, $chargePrice->grand_total_cent);
+        $this->assertEquals('EUR', $chargePrice->currency);
+
+        $testOrder = new TestOrder(['test_order_id' => 22]);
+        $chargePrice = new ChargePrice();
+        $chargePrice->model_type = 'TEST_ORDER';
+        $chargePrice->model_id = $testOrder->test_order_id;
+        $chargePrice->charge_type = 'MOCK_CHARGE_T1';
+        $chargePrice = $calculator->calculate($testOrder, $chargePrice);
+        $chargePrice->save(false);
+        $this->assertEquals(2, $chargePrice->price_table_id);
+        $this->assertEquals(1, $chargePrice->qty);
+        $this->assertEquals(270, $chargePrice->price_cent);
+        $this->assertEquals(270, $chargePrice->subtotal_cent);
+        $this->assertEquals(54, $chargePrice->discount_cent);
+        $this->assertEquals(216, $chargePrice->grand_total_cent);
+        $this->assertEquals('EUR', $chargePrice->currency);
+
+        $testOrder = new TestOrder(['test_order_id' => 3]);
+        $chargePrice = new ChargePrice();
+        $chargePrice->model_type = 'TEST_ORDER';
+        $chargePrice->model_id = $testOrder->test_order_id;
+        $chargePrice->charge_type = 'MOCK_CHARGE_T1';
+        $chargePrice = $calculator->calculate($testOrder, $chargePrice);
+        $chargePrice->save(false);
+        $this->assertEquals(3, $chargePrice->price_table_id);
+        $this->assertEquals(1, $chargePrice->qty);
+        $this->assertEquals(480, $chargePrice->price_cent);
+        $this->assertEquals(480, $chargePrice->subtotal_cent);
+        $this->assertEquals(144, $chargePrice->discount_cent);
+        $this->assertEquals(336, $chargePrice->grand_total_cent);
+        $this->assertEquals('EUR', $chargePrice->currency);
+
+        $testOrder = new TestOrder(['test_order_id' => 4]);
+        $chargePrice = new ChargePrice();
+        $chargePrice->model_type = 'TEST_ORDER';
+        $chargePrice->model_id = $testOrder->test_order_id;
+        $chargePrice->charge_type = 'MOCK_CHARGE_T1';
+        $chargePrice = $calculator->calculate($testOrder, $chargePrice);
+        $chargePrice->save(false);
+        $this->assertEquals(4, $chargePrice->price_table_id);
+        $this->assertEquals(2, $chargePrice->qty);
+        $this->assertEquals(780, $chargePrice->price_cent);
+        $this->assertEquals(1560, $chargePrice->subtotal_cent);
+        $this->assertEquals(624, $chargePrice->discount_cent);
+        $this->assertEquals(936, $chargePrice->grand_total_cent);
         $this->assertEquals('EUR', $chargePrice->currency);
     }
 }

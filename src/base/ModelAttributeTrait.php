d59107169f90b397d2a01e2bae944ae75b2838a0
Lujie <gao_lujie@live.cn>
2020-08-18 14:55:11 +0800

    fix

---
+++ src/base/ModelAttributeTrait.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\extend\base;
+
+/**
+ * Trait ModelAttributeTrait
+ * @package lujie\extend\base
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+trait ModelAttributeTrait
+{
+    /**
+     * @var array attribute values indexed by attribute names
+     */
+    private $_attributes = [];
+
+    /**
+     * @param string $name
+     * @return bool
+     */
+    public function hasAttribute(string $name): bool
+    {
+        return isset($this->_attributes[$name]) || in_array($name, $this->attributes(), true);
+    }
+
+    /**
+     * @param $name
+     * @return mixed|null
+     */
+    public function __get($name)
+    {
+        if (isset($this->_attributes[$name]) || array_key_exists($name, $this->_attributes)) {
+            return $this->_attributes[$name];
+        }
+        if ($this->hasAttribute($name)) {
+            return null;
+        }
+        return parent::__get($name);
+    }
+
+    /**
+     * @param $name
+     * @param $value
+     */
+    public function __set($name, $value)
+    {
+        if ($this->hasAttribute($name)) {
+            $this->_attributes[$name] = $value;
+        } else {
+            parent::__set($name, $value);
+        }
+    }
+}

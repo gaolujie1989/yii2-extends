86632b84f6c760fcf55d9472d9639fff86c081a2
Lujie <gao_lujie@live.cn>
2020-08-21 11:52:28 +0800

    fix

--- tests/unit/fixtures/data/charge_table.php
+++ tests/unit/fixtures/data/charge_table.php
@@ -19,7 +19,9 @@ return [
         'per_limit' => 0,
         'min_over_limit' => 0,
         'max_over_limit' => 0,
-        'additional' => [],
+        'additional' => [
+            'discountPriceCent' => 10,
+        ],
         'started_at' => strtotime('-7 days'),
         'ended_at' => strtotime('+7 days'),
         'owner_id' => 1,

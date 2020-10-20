9e63049c7c80b9057bad2cf07c086eebfba52894
Lujie <gao_lujie@live.cn>
2020-10-20 14:21:46 +0800

    fix

--- src/helpers/HttpClientHelper.php
+++ src/helpers/HttpClientHelper.php
@@ -34,7 +34,7 @@ class HttpClientHelper
         $response = $request->send();
 
         if (!$response->getIsOk() && !in_array($response->getStatusCode(), $allowedStatusCodes)) {
-            $message = 'Request failed with code: ' . $response->getStatusCode() . ', message: ' . $response->getContent();
+            $message = 'Request failed with code: ' . $response->getStatusCode();
             throw new InvalidResponseException($response, $message);
         }
 

861c112724115ac654db5e1e9cdfb1352440493f
Lujie <gao_lujie@live.cn>
2020-11-06 17:38:17 +0800

    fix

---
+++ src/account/migrations/m201106_170405_account.php
@@ -0,0 +1,29 @@
+<?php
+
+use lujie\extend\db\DropTableTrait;
+use lujie\extend\db\TraceableColumnTrait;
+use yii\db\Migration;
+
+class m190708_112313_fulfillment_account extends Migration
+{
+    use DropTableTrait, TraceableColumnTrait;
+
+    public $tableName = '{{%fulfillment_account}}';
+
+    public function safeUp()
+    {
+        $this->createTable($this->tableName, [
+            'fulfillment_account_id' => $this->bigPrimaryKey(),
+            'name' => $this->string(100)->notNull()->defaultValue(''),
+            'type' => $this->string(50)->notNull()->defaultValue(''),
+            'url' => $this->string()->notNull()->defaultValue(''),
+            'username' => $this->string()->notNull()->defaultValue(''),
+            'password' => $this->string()->notNull()->defaultValue(''),
+            'options' => $this->json(),
+            'additional' => $this->json(),
+            'status' => $this->tinyInteger()->notNull()->defaultValue(0),
+        ]);
+
+        $this->createIndex('idx_name', $this->tableName, ['name']);
+    }
+}

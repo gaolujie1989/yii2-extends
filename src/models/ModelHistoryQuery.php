b59ad3e94994e35a2c394828a7a2c6e7bdc375a7
Lujie <gao_lujie@live.cn>
2020-11-09 16:10:41 +0800

    fix

--- src/models/ModelHistoryQuery.php
+++ src/models/ModelHistoryQuery.php
@@ -2,7 +2,9 @@
 
 namespace lujie\ar\history\models;
 
+use lujie\common\account\models\Account;
 use lujie\db\fieldQuery\behaviors\FieldQueryBehavior;
+use yii\db\ActiveQuery;
 
 /**
  * This is the ActiveQuery class for [[History]].
@@ -45,4 +47,14 @@ class ModelHistoryQuery extends \yii\db\ActiveQuery
         ];
     }
 
+    /**
+     * @return ActiveQuery
+     * @inheritdoc
+     */
+    public function clearWhereAppendOnCondition(): ActiveQuery
+    {
+        /** @var Account $modelClass */
+        $modelClass = $this->modelClass;
+        return $this->where([])->andOnCondition(['model_type' => $modelClass::MODEL_TYPE]);
+    }
 }

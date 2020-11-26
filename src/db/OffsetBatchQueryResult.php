06b4106047022498236ff8f8af12628cccc6e430
Lujie <gao_lujie@live.cn>
2020-11-26 11:31:51 +0800

    fix

--- src/db/OffsetBatchQueryResult.php
+++ src/db/OffsetBatchQueryResult.php
@@ -5,6 +5,7 @@
 
 namespace lujie\extend\db;
 
+use Yii;
 use yii\base\InvalidValueException;
 
 /**
@@ -46,6 +47,7 @@ class OffsetBatchQueryResult extends \yii\db\BatchQueryResult
     {
         parent::init();
         $this->_queryCount = $this->query->count();
+        Yii::debug('Using OffsetBatchQueryResult, be sure that condition result and sort will not be changed in query', __METHOD__);
     }
 
     /**
@@ -86,7 +88,7 @@ class OffsetBatchQueryResult extends \yii\db\BatchQueryResult
         $this->_fetchCount += $offsetDataCount;
         if ($this->_fetchCount > $this->_queryCount
             || ($offsetDataCount === 0 && $this->_fetchCount < $this->_queryCount)) {
-            throw new InvalidValueException("This query with offset batch is invalid, query count {$this->_queryCount} is not equal with real fetched count {$this->_fetchCount}.");
+            Yii::error("OffsetBatchQueryResult query count {$this->_queryCount} is not equal with real fetched count {$this->_fetchCount}.", __METHOD__);
         }
         if ($offsetDataCount === 0) {
             return [];

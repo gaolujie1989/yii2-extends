a893cef45d35608001cffb3c6868ca33097216c8
Lujie <gao_lujie@live.cn>
2020-11-27 15:28:39 +0800

    fix

--- src/models/FulfillmentAccountQuery.php
+++ src/models/FulfillmentAccountQuery.php
@@ -5,10 +5,25 @@
 
 namespace lujie\fulfillment\models;
 
+use lujie\common\account\models\Account;
 use lujie\common\account\models\AccountQuery;
 
 /**
  * This is the ActiveQuery class for [[Account]].
+ *
+ * @method FulfillmentAccountQuery id($id)
+ * @method FulfillmentAccountQuery orderById($sort = SORT_ASC)
+ * @method int getId()
+ * @method array getIds()
+ *
+ * @method FulfillmentAccountQuery accountId($accountId)
+ * @method FulfillmentAccountQuery modelType($modelType)
+ * @method FulfillmentAccountQuery type($type)
+ * @method FulfillmentAccountQuery status($status)
+ * @method FulfillmentAccountQuery name($name)
+ *
+ * @method FulfillmentAccountQuery active()
+ * @method FulfillmentAccountQuery inActive()
  *
  * @method array|FulfillmentAccount[] all($db = null)
  * @method array|FulfillmentAccount|null one($db = null)

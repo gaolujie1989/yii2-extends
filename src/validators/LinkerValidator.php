5e57f4ea12d5de1ba616d5f8a36748c2595e5bd8
Lujie <gao_lujie@live.cn>
2020-12-03 20:57:16 +0800

    fix

---
+++ src/validators/LinkerValidator.php
@@ -0,0 +1,142 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\extend\validators;
+
+use Yii;
+use yii\db\ActiveQuery;
+use yii\db\BaseActiveRecord;
+use yii\db\Connection;
+use yii\validators\Validator;
+
+/**
+ * Class LinkerValidator
+ * @package lujie\extend\validators
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class LinkerValidator extends Validator
+{
+    /**
+     * @var string|BaseActiveRecord
+     */
+    public $targetClass;
+
+    /**
+     * @var string
+     */
+    public $targetAttribute;
+
+    /**
+     * @var string
+     */
+    public $targetRelation;
+
+    /**
+     * ['targetAttribute' => 'modelAttribute']
+     * @var array
+     */
+    public $linkAttributes;
+
+    /**
+     * @var string|array|callable
+     */
+    public $filter;
+
+    /**
+     * @var bool
+     */
+    public $forceMasterDb = true;
+
+    /**
+     * @var bool
+     */
+    public $checkExists = true;
+
+    /**
+     * @inheritdoc
+     */
+    public function init(): void
+    {
+        parent::init();
+        if ($this->message === null) {
+            $this->message = Yii::t('yii', '{attribute} is invalid.');
+        }
+    }
+
+    /**
+     * @param \yii\base\Model $model
+     * @param string $attribute
+     * @throws \Throwable
+     * @inheritdoc
+     */
+    public function validateAttribute($model, $attribute)
+    {
+        if ($this->targetRelation) {
+            /** @var ActiveQuery $activeQuery */
+            $activeQuery = $model->{'get' . ucfirst($this->targetRelation)}();
+        } else {
+            $targetAttribute = $this->targetAttribute ?: $attribute;
+            if (is_array($targetAttribute)) {
+                $condition = [];
+                foreach ($targetAttribute as $k => $v) {
+                    $condition[$v] = is_int($k) ? $model->$v : $model->$k;
+                }
+            } else {
+                $condition = [$targetAttribute => $model->$attribute];
+            }
+
+            /** @var BaseActiveRecord $targetClass */
+            $targetClass = $this->targetClass ?: get_class($model);
+            $activeQuery = $targetClass::find()->andWhere($condition);
+        }
+
+        $this->filterQuery($activeQuery);
+        $queryData = $this->queryData($activeQuery);
+        if ($queryData) {
+            foreach ($this->linkAttributes as $targetAttribute => $modelAttribute) {
+                $model->{$modelAttribute} = $queryData[$targetAttribute] ?? null;
+            }
+        } else if ($this->checkExists) {
+            $this->addError($model, $attribute, $this->message);
+        }
+    }
+
+    /**
+     * @param ActiveQuery $query
+     * @inheritdoc
+     */
+    protected function filterQuery(ActiveQuery $query): void
+    {
+        if (empty($this->filter)) {
+            return;
+        }
+        if (is_callable($this->filter)) {
+            call_user_func($this->filter, $query);
+        } else {
+            $query->andWhere($this->filter);
+        }
+    }
+
+    /**
+     * @param ActiveQuery $query
+     * @return array|null
+     * @throws \Throwable
+     * @inheritdoc
+     */
+    protected function queryData(ActiveQuery $query): ?array
+    {
+        /** @var BaseActiveRecord $modelClass */
+        $modelClass = $query->modelClass;
+        $db = $modelClass::getDb();
+        $query->asArray();
+        if ($this->forceMasterDb && $db instanceof Connection) {
+            return $db->useMaster(function () use ($query) {
+                return $query->one() ?: null;
+            });
+        } else {
+            return $query->one() ?: null;
+        }
+    }
+}

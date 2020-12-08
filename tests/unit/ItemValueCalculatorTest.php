30660602ce0d4e4dbce3826ba07ff0aedc2315a4
Lujie <gao_lujie@live.cn>
2020-12-08 18:16:59 +0800

    fix

---
+++ tests/unit/ItemValueCalculatorTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\fulfillment\tests\unit;
+
+
+class ItemValueCalculatorTest extends \Codeception\Test\Unit
+{
+    /**
+     * @var \UnitTester
+     */
+    protected $tester;
+
+    protected function _before()
+    {
+    }
+
+    protected function _after()
+    {
+    }
+
+    public function _fixtures(): array
+    {
+        return [
+
+        ];
+    }
+
+    /**
+     * @throws \yii\base\InvalidConfigException
+     * @inheritdoc
+     */
+    public function testMe()
+    {
+
+    }
+}

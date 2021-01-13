eb66a9a3e821f356c1f0e866b442f87486602123
Lujie <gao_lujie@live.cn>
2021-01-13 13:31:21 +0800

    fix

--- src/controllers/rest/TaskController.php
+++ src/controllers/rest/TaskController.php
@@ -5,7 +5,9 @@
 
 namespace lujie\project\controllers\rest;
 
+use lujie\batch\BatchAction;
 use lujie\extend\rest\ActiveController;
+use lujie\project\forms\TaskBatchForm;
 use lujie\project\models\Task;
 
 /**
@@ -16,4 +18,22 @@ use lujie\project\models\Task;
 class TaskController extends ActiveController
 {
     public $modelClass = Task::class;
+
+    /**
+     * @return array
+     * @throws \yii\base\InvalidConfigException
+     * @inheritdoc
+     */
+    public function actions(): array
+    {
+        return array_merge(parent::actions(), [
+            'batch-update' => [
+                'class' => BatchAction::class,
+                'modelClass' => $this->formClass,
+                'checkAccess' => [$this, 'checkAccess'],
+                'batchFormClass' => TaskBatchForm::class,
+                'method' => 'batchUpdate'
+            ]
+        ]);
+    }
 }

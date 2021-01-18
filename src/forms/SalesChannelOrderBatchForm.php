4a0abf8c39f3a388e73622d830b0940d15ac2d23
Lujie <gao_lujie@live.cn>
2021-01-18 16:45:35 +0800

    fix

---
+++ src/forms/SalesChannelOrderBatchForm.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\sales\channel\forms;
+
+
+use lujie\batch\BatchForm;
+
+/**
+ * Class SalesChannelOrderBatchForm
+ * @package lujie\sales\channel\forms
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class SalesChannelOrderBatchForm extends BatchForm
+{
+    /**
+     * @return array
+     * @inheritdoc
+     */
+    public function attributes(): array
+    {
+        return ['sales_channel_status'];
+    }
+
+    /**
+     * @return array
+     * @inheritdoc
+     */
+    public function rules(): array
+    {
+        return [
+            [['sales_channel_status'], 'safe']
+        ];
+    }
+}

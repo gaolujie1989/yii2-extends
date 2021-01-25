3701da2db6041286d75403c4bd61e259a3aab187
Lujie <gao_lujie@live.cn>
2021-01-25 09:46:25 +0800

    fix

--- src/f4px/F4pxFulfillmentService.php
+++ src/f4px/F4pxFulfillmentService.php
@@ -452,7 +452,14 @@ class F4pxFulfillmentService extends BaseFulfillmentService
      */
     public function cancelFulfillmentOrder(FulfillmentOrder $fulfillmentOrder): bool
     {
-        $cancelOutbound = $this->client->cancelOutbound(['consignment_no' => $fulfillmentOrder->external_order_key, 'cancel_type' => 'BC']);
+        if ($fulfillmentOrder->external_order_key) {
+            $cancelOutbound = $this->client->cancelOutbound(['consignment_no' => $fulfillmentOrder->external_order_key, 'cancel_type' => 'BC']);
+        } else {
+            $cancelOutbound = [
+                'consignment_no' => '',
+                'status' => 'X',
+            ];
+        }
         $this->updateFulfillmentOrder($fulfillmentOrder, $cancelOutbound, true);
         return $cancelOutbound['status'] === 'X';
     }

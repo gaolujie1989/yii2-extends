b6da9b0cb4e3d667f3f8a4b30832656d43edbf54
Lujie <gao_lujie@live.cn>
2021-03-09 15:55:22 +0800

    fix with search/form trait

--- src/searches/FulfillmentOrderSearch.php
+++ src/searches/FulfillmentOrderSearch.php
@@ -5,8 +5,8 @@
 
 namespace lujie\fulfillment\searches;
 
+use lujie\extend\base\SearchTrait;
 use lujie\fulfillment\models\FulfillmentOrder;
-use lujie\fulfillment\models\FulfillmentOrderQuery;
 
 /**
  * Class FulfillmentItemSearch
@@ -15,35 +15,5 @@ use lujie\fulfillment\models\FulfillmentOrderQuery;
  */
 class FulfillmentOrderSearch extends FulfillmentOrder
 {
-    /**
-     * @return array
-     * @inheritdoc
-     */
-    public function rules(): array
-    {
-        return [
-            [['fulfillment_account_id', 'fulfillment_status',
-                'order_id', 'order_status',
-                'external_order_key', 'external_order_status',
-                'order_pushed_status'], 'safe']
-        ];
-    }
-
-    /**
-     * @return FulfillmentOrderQuery
-     * @inheritdoc
-     */
-    public function query(): FulfillmentOrderQuery
-    {
-        return static::find()
-            ->andFilterWhere([
-                'fulfillment_account_id' => $this->fulfillment_account_id,
-                'fulfillment_status' => $this->fulfillment_status,
-                'order_id' => $this->order_id,
-                'order_status' => $this->order_status,
-                'external_order_key' => $this->external_order_key,
-                'external_order_status' => $this->external_order_status,
-                'order_pushed_status' => $this->order_pushed_status,
-            ]);
-    }
+    use SearchTrait;
 }
--- src/searches/FulfillmentWarehouseSearch.php
+++ src/searches/FulfillmentWarehouseSearch.php
@@ -5,9 +5,9 @@
 
 namespace lujie\fulfillment\searches;
 
-use lujie\alias\behaviors\TimestampAliasBehavior;
+use lujie\extend\base\SearchTrait;
+use lujie\extend\helpers\ModelHelper;
 use lujie\fulfillment\models\FulfillmentWarehouse;
-use lujie\fulfillment\models\FulfillmentWarehouseQuery;
 
 /**
  * Class FulfillmentItemSearch
@@ -16,55 +16,17 @@ use lujie\fulfillment\models\FulfillmentWarehouseQuery;
  */
 class FulfillmentWarehouseSearch extends FulfillmentWarehouse
 {
-    /**
-     * @return array
-     * @inheritdoc
-     */
-    public function rules(): array
-    {
-        return [
-            [['fulfillment_account_id', 'warehouse_id', 'external_warehouse_key'], 'safe']
-        ];
-    }
+    use SearchTrait;
 
     /**
-     * @return FulfillmentWarehouseQuery
+     * @param array $row
+     * @return mixed
+     * @throws \Exception
      * @inheritdoc
      */
-    public function query(): FulfillmentWarehouseQuery
+    public static function prepareArray(array $row): array
     {
-        return static::find()
-            ->andFilterWhere([
-                'fulfillment_account_id' => $this->fulfillment_account_id,
-                'warehouse_id' => $this->warehouse_id,
-                'external_warehouse_key' => $this->external_warehouse_key
-            ]);
-    }
-
-    /**
-     * @return array
-     * @inheritdoc
-     */
-    public function behaviors(): array
-    {
-        return array_merge(parent::behaviors(), [
-            'tsAlias' => [
-                'class' => TimestampAliasBehavior::class,
-                'aliasProperties' => [
-                    'external_movement_time' => 'external_movement_at',
-                ]
-            ]
-        ]);
-    }
-
-    /**
-     * @return array
-     * @inheritdoc
-     */
-    public function fields(): array
-    {
-        return array_merge(parent::fields(), [
-            'external_movement_time' => 'external_movement_time',
-        ]);
+        $alias = ['external_movement_time' => 'external_movement_time'];
+        return ModelHelper::prepareArray($row, static::class, $alias);
     }
 }

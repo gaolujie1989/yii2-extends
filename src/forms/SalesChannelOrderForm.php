2c428cf09454499d1e5339e136ce51e0353b9b50
Lujie <gao_lujie@live.cn>
2021-03-09 15:55:21 +0800

    fix with search/form trait

--- src/forms/SalesChannelOrderForm.php
+++ src/forms/SalesChannelOrderForm.php
@@ -6,8 +6,8 @@
 namespace lujie\sales\channel\forms;
 
 
+use lujie\extend\base\FormTrait;
 use lujie\sales\channel\models\SalesChannelOrder;
-use lujie\sales\channel\models\SalesChannelOrderQuery;
 
 /**
  * Class SalesChannelOrderForm
@@ -16,33 +16,5 @@ use lujie\sales\channel\models\SalesChannelOrderQuery;
  */
 class SalesChannelOrderForm extends SalesChannelOrder
 {
-    /**
-     * @return array
-     * @inheritdoc
-     */
-    public function rules(): array
-    {
-        return [
-            [['sales_channel_account_id', 'sales_channel_status',
-                'order_id', 'order_status',
-                'external_order_key', 'external_order_status'], 'safe']
-        ];
-    }
-
-    /**
-     * @return SalesChannelOrderQuery
-     * @inheritdoc
-     */
-    public function query(): SalesChannelOrderQuery
-    {
-        return static::find()
-            ->andFilterWhere([
-                'sales_channel_account_id' => $this->sales_channel_account_id,
-                'sales_channel_status' => $this->sales_channel_status,
-                'order_id' => $this->order_id,
-                'order_status' => $this->order_status,
-                'external_order_key' => $this->external_order_key,
-                'external_order_status' => $this->external_order_status,
-            ]);
-    }
+    use FormTrait;
 }
--- src/searches/SalesChannelOrderSearch.php
+++ src/searches/SalesChannelOrderSearch.php
@@ -5,8 +5,8 @@
 
 namespace lujie\sales\channel\searches;
 
+use lujie\extend\base\SearchTrait;
 use lujie\sales\channel\models\SalesChannelOrder;
-use lujie\sales\channel\models\SalesChannelOrderQuery;
 
 /**
  * Class SalesChannelOrderSearch
@@ -15,34 +15,5 @@ use lujie\sales\channel\models\SalesChannelOrderQuery;
  */
 class SalesChannelOrderSearch extends SalesChannelOrder
 {
-    /**
-     * @return array
-     * @inheritdoc
-     */
-    public function rules(): array
-    {
-        return [
-            [['sales_channel_account_id', 'sales_channel_status',
-                'order_id', 'order_status',
-                'external_order_key', 'external_order_status',
-                'order_pushed_status'], 'safe'],
-        ];
-    }
-
-    /**
-     * @return SalesChannelOrderQuery
-     * @inheritdoc
-     */
-    public function query(): SalesChannelOrderQuery
-    {
-        return static::find()->andFilterWhere([
-            'sales_channel_account_id' => $this->sales_channel_account_id,
-            'sales_channel_status' => $this->sales_channel_status,
-            'order_id' => $this->order_id,
-            'order_status' => $this->order_status,
-            'external_order_key' => $this->external_order_key,
-            'external_order_status' => $this->external_order_status,
-            'order_pushed_status' => $this->order_pushed_status,
-        ]);
-    }
+    use SearchTrait;
 }

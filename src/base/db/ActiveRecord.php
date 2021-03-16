5731991566f382a69bf0c6158eaf14499a030d2a
Lujie <gao_lujie@live.cn>
2021-03-16 18:02:56 +0800

    fix form/search trait

--- src/base/db/ActiveRecord.php
+++ src/base/db/ActiveRecord.php
@@ -6,8 +6,8 @@
 namespace lujie\data\recording\base\db;
 
 
+use lujie\extend\db\AliasFieldTrait;
 use lujie\extend\db\DbConnectionTrait;
-use lujie\extend\db\IdFieldTrait;
 use lujie\extend\db\SaveTrait;
 use lujie\extend\db\TraceableBehaviorTrait;
 use lujie\extend\db\TransactionTrait;
@@ -19,5 +19,5 @@ use lujie\extend\db\TransactionTrait;
  */
 class ActiveRecord extends \yii\db\ActiveRecord
 {
-    use TraceableBehaviorTrait, IdFieldTrait, SaveTrait, TransactionTrait, DbConnectionTrait;
+    use TraceableBehaviorTrait, AliasFieldTrait, SaveTrait, TransactionTrait, DbConnectionTrait;
 }

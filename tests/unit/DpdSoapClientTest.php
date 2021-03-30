b54497cec0d44f406e8a0e85df5fcfb3bd74204e
Lujie <gao_lujie@live.cn>
2021-03-30 11:44:04 +0800

    remove UnitTester

--- tests/unit/DpdSoapClientTest.php
+++ tests/unit/DpdSoapClientTest.php
@@ -17,10 +17,7 @@ use Yii;
 
 class DpdSoapClientTest extends \Codeception\Test\Unit
 {
-    /**
-     * @var \UnitTester
-     */
-    protected $tester;
+
 
     protected function _before()
     {

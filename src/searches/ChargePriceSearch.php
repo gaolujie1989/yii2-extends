a3fa086c51f8ba0460ca24c4f6704a0dd02a7ad1
Lujie <gao_lujie@live.cn>
2021-07-13 14:08:49 +0800

    fix db connection

--- src/searches/ChargePriceSearch.php
+++ src/searches/ChargePriceSearch.php
@@ -2,17 +2,8 @@
 
 namespace lujie\charging\searches;
 
-use lujie\alias\behaviors\AliasPropertyBehavior;
-use lujie\alias\behaviors\MoneyAliasBehavior;
 use lujie\charging\models\ChargePrice;
-use lujie\extend\db\AliasFieldTrait;
-use lujie\extend\db\DbConnectionTrait;
-use lujie\extend\db\SaveTrait;
 use lujie\extend\db\SearchTrait;
-use lujie\extend\db\TraceableBehaviorTrait;
-use lujie\extend\db\TransactionTrait;
-use Yii;
-use yii\db\ActiveQuery;
 
 /**
  * Class ChargePrice

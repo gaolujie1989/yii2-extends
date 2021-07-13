65fb14738827162453b28753bcb1ae2947b7c7ea
Lujie <gao_lujie@live.cn>
2021-07-13 14:05:30 +0800

    fix

--- src/base/db/ActiveRecord.php
+++ src/base/db/ActiveRecord.php
@@ -5,20 +5,11 @@
 
 namespace lujie\data\recording\base\db;
 
-use lujie\alias\behaviors\AliasBehaviorTrait;
-use lujie\extend\db\AliasFieldTrait;
-use lujie\extend\db\DbConnectionTrait;
-use lujie\extend\db\DeleteTrait;
-use lujie\extend\db\SaveTrait;
-use lujie\extend\db\TraceableBehaviorTrait;
-use lujie\extend\db\TransactionTrait;
-
 /**
  * Class ActiveRecord
  * @package lujie\data\recording\base\db
  * @author Lujie Zhou <gao_lujie@live.cn>
  */
-class ActiveRecord extends \yii\db\ActiveRecord
+class ActiveRecord extends \lujie\extend\db\ActiveRecord
 {
-    use TraceableBehaviorTrait, AliasBehaviorTrait, AliasFieldTrait, SaveTrait, DeleteTrait, TransactionTrait, DbConnectionTrait;
 }

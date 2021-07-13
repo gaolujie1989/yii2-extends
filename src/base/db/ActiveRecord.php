c456b2173374582f2264786502824e7a6d597848
Lujie <gao_lujie@live.cn>
2021-07-13 14:08:47 +0800

    fix db connection

--- src/base/db/ActiveRecord.php
+++ src/base/db/ActiveRecord.php
@@ -5,8 +5,6 @@
 
 namespace lujie\stock\base\db;
 
-use lujie\extend\db\DbConnectionTrait;
-
 /**
  * Class ActiveRecord
  * @package lujie\stock\base\db
@@ -14,5 +12,4 @@ use lujie\extend\db\DbConnectionTrait;
  */
 class ActiveRecord extends \lujie\extend\db\ActiveRecord
 {
-    use DbConnectionTrait;
 }

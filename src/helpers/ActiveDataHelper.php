2f1030b664de0c1b131b2dc4093d6bdf5ecb3947
Lujie <gao_lujie@live.cn>
2021-09-16 15:35:11 +0800

    fix

--- src/helpers/ActiveDataHelper.php
+++ src/helpers/ActiveDataHelper.php
@@ -5,7 +5,10 @@
 
 namespace lujie\extend\helpers;
 
+use Yii;
 use yii\db\ActiveRecord;
+use yii\db\ColumnSchema;
+use yii\db\Connection;
 use yii\helpers\ArrayHelper;
 
 /**
@@ -26,19 +29,45 @@ class ActiveDataHelper
     {
         if (is_subclass_of($modelClass, ActiveRecord::class)) {
             $columns = $modelClass::getTableSchema()->columns;
-            $closure = static function ($row) use ($columns) {
-                foreach ($row as $name => $value) {
-                    if (isset($columns[$name]) && is_string($value)) { //only string need phpTypecast, avoid phpTypecast twice cause error
-                        $row[$name] = $columns[$name]->phpTypecast($value);
-                    }
-                }
-                return $row;
-            };
-            if (ArrayHelper::isAssociative($data, false)) {
-                return $closure($data);
-            }
-            return array_map($closure, $data);
+            return static::phpTypecastInternal($data, $columns);
         }
         return $data;
+    }
+
+    /**
+     * @param array $data
+     * @param ColumnSchema[] $columns
+     * @return array
+     * @inheritdoc
+     */
+    protected static function phpTypecastInternal(array $data, array $columns): array
+    {
+        $closure = static function ($row) use ($columns) {
+            foreach ($row as $name => $value) {
+                if (isset($columns[$name]) && is_string($value)) { //only string need phpTypecast, avoid phpTypecast twice cause error
+                    $row[$name] = $columns[$name]->phpTypecast($value);
+                }
+            }
+            return $row;
+        };
+        if (ArrayHelper::isAssociative($data, false)) {
+            return $closure($data);
+        }
+        return array_map($closure, $data);
+    }
+
+    /**
+     * @param array $data
+     * @param string $table
+     * @param Connection|null $db
+     * @return array
+     * @throws \yii\base\NotSupportedException
+     * @inheritdoc
+     */
+    public static function phpTypecast(array $data, string $table, Connection $db = null): array
+    {
+        $db = $db ?: Yii::$app->getDb();
+        $columns = $db->getSchema()->getTableSchema($table)->columns;
+        return static::phpTypecastInternal($data, $columns);
     }
 }

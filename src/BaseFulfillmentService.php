ce76d65cf69bed41d7be6a22d7485c58738dad76
Lujie <gao_lujie@live.cn>
2021-11-16 09:29:48 +0800

    fix

--- src/BaseFulfillmentService.php
+++ src/BaseFulfillmentService.php
@@ -702,7 +702,8 @@ abstract class BaseFulfillmentService extends Component implements FulfillmentSe
             ->indexBy('charge_type')
             ->all();
         $updatedChargePrices = [];
-        foreach ($externalOrderCharges as $chargeType => $chargeData) {
+        foreach ($externalOrderCharges as $chargeData) {
+            $chargeType = $chargeData['charge_type'];
             $chargePrice = $chargePrices[$chargeType] ?? new ChargePrice();
             $chargePrice->model_type = FulfillmentConst::FULFILLMENT_CHARGE_MODEL;
             $chargePrice->model_id = $fulfillmentOrder->fulfillment_order_id;

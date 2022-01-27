278331e6dc1d48366f73014f3759ad9455c2a320
Lujie <gao_lujie@live.cn>
2022-01-27 09:39:45 +0800

    fix

--- src/validators/DateValidator.php
+++ src/validators/DateValidator.php
@@ -16,6 +16,7 @@ use yii\base\InvalidConfigException;
 class DateValidator extends \yii\validators\DateValidator
 {
     public const TIMESTAMP_ATTRIBUTE_SELF = 'SELF';
+    public const TIMESTAMP_ATTRIBUTE_FORMAT_AUTO = 'AUTO';
     public const TIMESTAMP_ATTRIBUTE_FORMAT_BY_TYPE = 'BY_TYPE';
 
     /**
@@ -27,6 +28,11 @@ class DateValidator extends \yii\validators\DateValidator
      * @var string
      */
     public $timestampAttribute = self::TIMESTAMP_ATTRIBUTE_SELF;
+
+    /**
+     * @var string
+     */
+    public $timestampAttributeFormat = self::TIMESTAMP_ATTRIBUTE_FORMAT_AUTO;
 
     /**
      * @var string
@@ -57,6 +63,23 @@ class DateValidator extends \yii\validators\DateValidator
     }
 
     /**
+     * @param string $attribute
+     * @inheritdoc
+     */
+    private function setAutoTimestampAttributeFormat(string $attribute): void
+    {
+        if (substr($attribute, -3) === '_at') {
+            $this->timestampAttributeFormat = null;
+        } else if (substr($attribute, -5) === '_date') {
+            $this->timestampAttributeFormat = 'php:Y-m-d';
+        } else if (substr($attribute, -9) === '_datetime') {
+            $this->timestampAttributeFormat = 'php:Y-m-d H:i:s';
+        } else if (substr($attribute, -5) === '_time') {
+            $this->timestampAttributeFormat = 'php:H:i:s';
+        }
+    }
+
+    /**
      * @inheritdoc
      */
     protected function parseDateValue($value)
@@ -77,12 +100,22 @@ class DateValidator extends \yii\validators\DateValidator
      */
     public function validateAttribute($model, $attribute): void
     {
-        if ($this->timestampAttribute === self::TIMESTAMP_ATTRIBUTE_SELF) {
+        $isAutoFormat = ($this->timestampAttributeFormat === self::TIMESTAMP_ATTRIBUTE_FORMAT_AUTO);
+        $isSelfAttribute = ($this->timestampAttribute === self::TIMESTAMP_ATTRIBUTE_SELF);
+        if ($isAutoFormat) {
+            $this->setAutoTimestampAttributeFormat($attribute);
+        }
+        if ($isSelfAttribute) {
             $this->timestampAttribute = $attribute;
-            parent::validateAttribute($model, $attribute);
+        }
+
+        parent::validateAttribute($model, $attribute);
+
+        if ($isAutoFormat) {
+            $this->timestampAttributeFormat = self::TIMESTAMP_ATTRIBUTE_FORMAT_AUTO;
+        }
+        if ($isSelfAttribute) {
             $this->timestampAttribute = self::TIMESTAMP_ATTRIBUTE_SELF;
-        } else {
-            parent::validateAttribute($model, $attribute);
         }
     }
 }

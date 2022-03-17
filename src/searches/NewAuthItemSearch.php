09a9930a58a23e12df0f02e285e70c566cd53970
Lujie <gao_lujie@live.cn>
2022-03-17 11:46:54 +0800

    fix

--- src/searches/NewAuthItemSearch.php
+++ src/searches/NewAuthItemSearch.php
@@ -7,6 +7,7 @@ namespace lujie\auth\searches;
 
 use lujie\auth\models\NewAuthItem;
 use lujie\extend\db\SearchTrait;
+use yii\db\ActiveQueryInterface;
 
 /**
  * @copyright Copyright (c) 2019
@@ -14,4 +15,25 @@ use lujie\extend\db\SearchTrait;
 class NewAuthItemSearch extends NewAuthItem
 {
     use SearchTrait;
+
+    /**
+     * @return ActiveQueryInterface
+     * @inheritdoc
+     */
+    public function query(): ActiveQueryInterface
+    {
+        $columns = array_diff($this->attributes(), ['data']);
+        return $this->searchQuery()->select($columns);
+    }
+
+    /**
+     * @return array
+     * @inheritdoc
+     */
+    public function fields(): array
+    {
+        $fields = parent::fields();
+        unset($fields['data']);
+        return $fields;
+    }
 }
--- src/searches/NewAuthRuleSearch.php
+++ src/searches/NewAuthRuleSearch.php
@@ -7,6 +7,7 @@ namespace lujie\auth\searches;
 
 use lujie\auth\models\NewAuthRule;
 use lujie\extend\db\SearchTrait;
+use yii\db\ActiveQueryInterface;
 
 /**
  * Class NewAuthRuleSearch
@@ -16,4 +17,25 @@ use lujie\extend\db\SearchTrait;
 class NewAuthRuleSearch extends NewAuthRule
 {
     use SearchTrait;
+
+    /**
+     * @return ActiveQueryInterface
+     * @inheritdoc
+     */
+    public function query(): ActiveQueryInterface
+    {
+        $columns = array_diff($this->attributes(), ['data']);
+        return $this->searchQuery()->select($columns);
+    }
+
+    /**
+     * @return array
+     * @inheritdoc
+     */
+    public function fields(): array
+    {
+        $fields = parent::fields();
+        unset($fields['data']);
+        return $fields;
+    }
 }

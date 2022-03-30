588e9cae3931e42d84b20707de417fe34abfe48f
Lujie <gao_lujie@live.cn>
2022-03-30 14:42:38 +0800

    fix

--- src/Yii2WebServer.php
+++ src/Yii2WebServer.php
@@ -5,18 +5,9 @@
 
 namespace lujie\workerman;
 
-use lujie\workerman\db\Command;
-use lujie\workerman\db\Connection;
-use lujie\workerman\log\Logger;
-use lujie\workerman\web\ErrorHandler;
 use Workerman\Connection\TcpConnection;
-use Workerman\Protocols\Http;
 use Workerman\WebServer;
-use Workerman\Worker;
-use Yii;
-use yii\db\Connection as YiiConnection;
-use yii\web\Application;
-use yii\web\UploadedFile;
+use yii\base\InvalidConfigException;
 
 /**
  * Class Yii2WebServer
@@ -26,100 +17,22 @@ use yii\web\UploadedFile;
 class Yii2WebServer extends WebServer
 {
     /**
-     * @var string
+     * @var Yii2WorkerHandler
      */
-    public $yii2AppFile = 'app.php';
+    public $yii2WorkerHandler;
 
     /**
-     * @var Application[]
-     */
-    public $yii2Apps = [];
-
-    /**
-     * @var array
-     */
-    public $yii2AppUrlPrefix = [
-        '!/uploads',
-    ];
-
-    public $yii2AppUrlDefault = true;
-
-    /**
-     * @var string
-     */
-    public $uploadTmpDir = '/tmp';
-
-    /**
-     * @var int
-     */
-    public $maxRequestCount = 10000;
-
-    /**
-     * @var int
-     */
-    private $currentRequestCount = 0;
-
-    /**
-     * Run webserver instance.
-     *
-     * @see Workerman.Worker::run()
-     */
-    public function run(): void
-    {
-        $this->uploadTmpDir = rtrim($this->uploadTmpDir, '/') . '/';
-        parent::run();
-    }
-
-    /**
-     * @throws \Exception
+     * @throws InvalidConfigException
      * @inheritdoc
      */
     public function onWorkerStart(): void
     {
-        $this->initYii2Apps();
-        XHProfiler::init();
-        parent::onWorkerStart();
-    }
-
-    /**
-     * @inheritdoc
-     */
-    protected function initYii2Apps(): void
-    {
-        include_once __DIR__ . '/rewrite_functions.php';
-        include_once __DIR__ . '/rewrite_classes.php';
-        $this->yii2Apps = [];
-        foreach ($this->serverRoot as $domain => $serverConfig) {
-            $domainRoot = $this->serverRoot[$domain]['root'];
-            $workermanCwd = getcwd();
-            chdir($domainRoot);
-
-            $_SERVER['SCRIPT_NAME'] = '/index.php';
-            $_SERVER['SCRIPT_FILENAME'] = $domainRoot . $_SERVER['SCRIPT_NAME'];
-            $appFile = rtrim($serverConfig['root'], '/') . '/' . $this->yii2AppFile;
-            $this->yii2Apps[$domain] = $yii2App = include $appFile;
-            foreach ($yii2App->getComponents() as $name => $config) {
-                if ($name === 'logger') {
-                    $config['class'] = Logger::class;
-                    $yii2App->setComponents([$name => $config]);
-                }
-                if ($name === 'errorHandler') {
-                    $config['class'] = ErrorHandler::class;
-                    $yii2App->setComponents([$name => $config]);
-                }
-                if (ltrim($config['class'], '\\') === YiiConnection::class) {
-                    $config['class'] = Connection::class;
-                    $config['commandClass'] = Command::class;
-                    $yii2App->setComponents([$name => $config]);
-                }
-                $yii2App->get($name);
-            }
-            foreach ($yii2App->getModules() as $name => $config) {
-                $yii2App->getModule($name);
-            }
-            $yii2App->getErrorHandler()->register();
-            chdir($workermanCwd);
+        if ($this->yii2WorkerHandler === null) {
+            $this->yii2WorkerHandler = new Yii2WorkerHandler();
+            $this->yii2WorkerHandler->serverRoot = $this->serverRoot;
         }
+        $this->yii2WorkerHandler->initYii2Apps();
+        parent::onWorkerStart();
     }
 
     /**
@@ -127,117 +40,11 @@ class Yii2WebServer extends WebServer
      * @throws \yii\base\ErrorException
      * @inheritdoc
      */
-    public function onMessage($connection): void
+    public function onMessage(TcpConnection $connection): void
     {
-        if ($this->maxRequestCount && ++$this->currentRequestCount > $this->maxRequestCount) {
-            Worker::stopAll();
-        }
-
-        // REQUEST_URI.
-        $workerman_url_info = parse_url('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
-        if (!$workerman_url_info) {
-            Http::header('HTTP/1.1 400 Bad Request');
-            $connection->close('<h1>400 Bad Request</h1>');
+        if ($this->yii2WorkerHandler->handleMessage($connection)) {
             return;
         }
-
-        $workerman_path = $workerman_url_info['path'] ?? '/';
-        if ($this->isYii2AppUrl($workerman_path)) {
-            $_SERVER['REMOTE_ADDR'] = $connection->getRemoteIp();
-            $_SERVER['REMOTE_PORT'] = $connection->getRemotePort();
-            $content = $this->runYii2App();
-            if (strtolower($_SERVER['HTTP_CONNECTION']) === 'keep-alive') {
-                $connection->send($content);
-            } else {
-                $connection->close($content);
-            }
-        } else {
-            parent::onMessage($connection);
-        }
-    }
-
-    /**
-     * @param string $urlPath
-     * @return bool
-     * @inheritdoc
-     */
-    protected function isYii2AppUrl(string $urlPath): bool
-    {
-        foreach ($this->yii2AppUrlPrefix as $urlPrefix) {
-            if (strpos($urlPrefix, '!') === 0) {
-                $urlPrefix = substr($urlPrefix, 1);
-                if (strpos($urlPath, $urlPrefix) === 0) {
-                    return false;
-                }
-            } elseif (strpos($urlPath, $urlPrefix) === 0) {
-                return true;
-            }
-        }
-        return $this->yii2AppUrlDefault;
-    }
-
-    /**
-     * @return string
-     * @throws \yii\base\ErrorException
-     * @inheritdoc
-     */
-    protected function runYii2App(): string
-    {
-        $yii2App = $this->yii2Apps[$_SERVER['SERVER_NAME']] ?? current($this->yii2Apps);
-        $workermanSiteConfig = $this->serverRoot[$_SERVER['SERVER_NAME']] ?? current($this->serverRoot);
-        $domainRoot = rtrim($workermanSiteConfig['root'], '/');
-        $workermanCwd = getcwd();
-        chdir($domainRoot);
-        ob_start();
-        // Try to run yii2 app.
-        try {
-            $_SERVER['REQUEST_TIME'] = time();
-            $_SERVER['REQUEST_TIME_FLOAT'] = microtime(true);
-            $_SERVER['SCRIPT_NAME'] = '/index.php';
-            $_SERVER['SCRIPT_FILENAME'] = $domainRoot . $_SERVER['SCRIPT_NAME'];
-            $this->setUploadedFiles();
-            $componentsConfig = $yii2App->getComponents();
-            $yii2App->set('request', $componentsConfig['request']);
-            $yii2App->set('response', $componentsConfig['response']);
-            $yii2App->getRequest()->setRawBody($GLOBALS['HTTP_RAW_POST_DATA']);
-            XHProfiler::start();
-            $yii2App->run();
-        } catch (\Exception $exception) {
-            if ($exception->getMessage() !== 'jump_exit') {
-                $yii2App->getErrorHandler()->handleException($exception);
-            }
-        } catch (\Throwable $error) {
-            $yii2App->getErrorHandler()->handleException($error);
-        } finally {
-            XHProfiler::end();
-        }
-        $content = ob_get_clean();
-        Yii::getLogger()->flush(true);
-        XHProfiler::save();
-        chdir($workermanCwd);
-        return $content;
-    }
-
-    /**
-     * @throws \Exception
-     * @inheritdoc
-     */
-    protected function setUploadedFiles(): void
-    {
-        $uploadFiles = $_FILES;
-        $_FILES = [];
-        foreach ($uploadFiles as $file) {
-            $tmpName = $this->uploadTmpDir . WORKERMAN_UPLOAD_FILENAME_PREFIX
-                . date('ymdHis') . '_' . random_int(1000, 9999);
-            file_put_contents($tmpName, $file['file_data']);
-            $_FILES[$file['name']] = [
-                'name' => $file['file_name'],
-                'type' => $file['file_type'] ?? '',
-                'tmp_name' => $tmpName,
-                'error' => UPLOAD_ERR_OK,
-                'size' => $file['file_size'],
-            ];
-        }
-        UploadedFile::reset();
+        parent::onMessage($connection);
     }
 }
---
+++ src/Yii2WorkerHandler.php
@@ -0,0 +1,255 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\workerman;
+
+use lujie\workerman\db\Command;
+use lujie\workerman\db\Connection;
+use lujie\workerman\log\Logger;
+use lujie\workerman\web\ErrorHandler;
+use Workerman\Connection\TcpConnection;
+use Workerman\Protocols\Http;
+use Workerman\Worker;
+use Yii;
+use yii\base\Application;
+use yii\base\ErrorException;
+use yii\base\InvalidConfigException;
+use yii\db\Connection as YiiConnection;
+use yii\web\UploadedFile;
+
+class Yii2WorkerHandler
+{
+    /**
+     * @var string
+     */
+    public $yii2AppFile = 'app.php';
+
+    /**
+     * @var Application[]
+     */
+    public $yii2Apps = [];
+
+    /**
+     * @var array
+     */
+    public $yii2AppUrlPrefix = [
+        '!/uploads',
+    ];
+
+    public $yii2AppUrlDefault = true;
+
+    /**
+     * @var string
+     */
+    public $uploadTmpDir = '/tmp';
+
+    /**
+     * Virtual host to path mapping.
+     *
+     * @var array ['workerman.net'=>['root' => '/home'], 'www.workerman.net'=>['root' => 'home/www']]
+     */
+    public $serverRoot = array();
+
+    #region Restart after max request
+
+    /**
+     * @var int
+     */
+    private $currentRequestCount = 0;
+
+    /**
+     * @var int
+     */
+    public $maxRequestCount = 10000;
+
+    /**
+     * @inheritdoc
+     */
+    protected function checkMaxRequestCount(): void
+    {
+        if ($this->maxRequestCount && ++$this->currentRequestCount > $this->maxRequestCount) {
+            Worker::stopAll();
+        }
+    }
+
+    #endregion
+
+    #region onWorkerStart
+
+    /**
+     * @throws InvalidConfigException
+     */
+    public function initYii2Apps(): void
+    {
+        include_once __DIR__ . '/rewrite_functions.php';
+        include_once __DIR__ . '/rewrite_classes.php';
+        $this->yii2Apps = [];
+        foreach ($this->serverRoot as $domain => $serverConfig) {
+            $domainRoot = is_string($serverConfig) ? $serverConfig : $serverConfig['root'];
+            $workermanCwd = getcwd();
+            chdir($domainRoot);
+            $appFile = rtrim($domainRoot, '/') . '/' . $this->yii2AppFile;
+            $this->yii2Apps[$domain] = ($yii2App = include $appFile);
+            $this->adaptYii2App($yii2App);
+            chdir($workermanCwd);
+        }
+    }
+
+    /**
+     * @param Application $app
+     * @throws InvalidConfigException
+     * @inheritdoc
+     */
+    public function adaptYii2App(Application $app): void
+    {
+        $_SERVER['SCRIPT_NAME'] = '/index.php';
+        $_SERVER['SCRIPT_FILENAME'] = getcwd() . $_SERVER['SCRIPT_NAME'];
+        foreach ($app->getComponents() as $name => $config) {
+            if ($name === 'logger') {
+                $config['class'] = Logger::class;
+                $app->setComponents([$name => $config]);
+            }
+            if ($name === 'errorHandler') {
+                $config['class'] = ErrorHandler::class;
+                $app->setComponents([$name => $config]);
+            }
+            if (ltrim($config['class'], '\\') === YiiConnection::class) {
+                $config['class'] = Connection::class;
+                $config['commandClass'] = Command::class;
+                $app->setComponents([$name => $config]);
+            }
+            $app->get($name);
+        }
+        foreach ($app->getModules() as $name => $config) {
+            $app->getModule($name);
+        }
+        $app->getErrorHandler()->register();
+    }
+
+    #endregion
+
+    #region onMessage
+
+    /**
+     * @param TcpConnection $connection
+     * @return bool
+     * @throws ErrorException
+     * @inheritdoc
+     */
+    public function handleMessage(TcpConnection $connection): bool
+    {
+        $this->checkMaxRequestCount();
+
+        // REQUEST_URI.
+        $urlInfo = parse_url('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+        if (!$urlInfo) {
+            Http::header('HTTP/1.1 400 Bad Request');
+            $connection->close('<h1>400 Bad Request</h1>');
+            return true;
+        }
+
+        $urlPath = $urlInfo['path'] ?? '/';
+        if ($this->isYii2AppUrl($urlPath)) {
+            $_SERVER['REMOTE_ADDR'] = $connection->getRemoteIp();
+            $_SERVER['REMOTE_PORT'] = $connection->getRemotePort();
+            $yii2App = $this->yii2Apps[$_SERVER['SERVER_NAME']] ?? current($this->yii2Apps);
+            $serverConfig = $this->serverRoot[$_SERVER['SERVER_NAME']] ?? current($this->serverRoot);
+            $domainRoot = is_string($serverConfig) ? $serverConfig : $serverConfig['root'];
+            $workermanCwd = getcwd();
+            chdir($domainRoot);
+            $content = $this->handleYii2AppRequest($yii2App);
+            chdir($workermanCwd);
+            if (strtolower($_SERVER['HTTP_CONNECTION']) === 'keep-alive') {
+                $connection->send($content);
+            } else {
+                $connection->close($content);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param Application $app
+     * @return string
+     * @throws ErrorException
+     * @inheritdoc
+     */
+    protected function handleYii2AppRequest(Application $app): string
+    {
+        ob_start();
+        // Try to run yii2 app.
+        try {
+            XHProfiler::start();
+            $_SERVER['REQUEST_TIME'] = time();
+            $_SERVER['REQUEST_TIME_FLOAT'] = microtime(true);
+            $_SERVER['SCRIPT_NAME'] = '/index.php';
+            $_SERVER['SCRIPT_FILENAME'] = getcwd() . $_SERVER['SCRIPT_NAME'];
+            $this->setUploadedFiles();
+            $componentsConfig = $app->getComponents();
+            $app->set('request', $componentsConfig['request']);
+            $app->set('response', $componentsConfig['response']);
+            $app->getRequest()->setRawBody($GLOBALS['HTTP_RAW_POST_DATA']);
+            $app->run();
+        } catch (\Exception $exception) {
+            if ($exception->getMessage() !== 'jump_exit') {
+                $app->getErrorHandler()->handleException($exception);
+            }
+        } catch (\Throwable $error) {
+            $app->getErrorHandler()->handleException($error);
+        } finally {
+            XHProfiler::end();
+            XHProfiler::save();
+        }
+        $content = ob_get_clean();
+        Yii::getLogger()->flush(true);
+        return $content;
+    }
+
+    /**
+     * @throws \Exception
+     * @inheritdoc
+     */
+    protected function setUploadedFiles(): void
+    {
+        $uploadFiles = $_FILES;
+        $_FILES = [];
+        $this->uploadTmpDir = rtrim($this->uploadTmpDir, '/') . '/';
+        foreach ($uploadFiles as $file) {
+            $tmpName = $this->uploadTmpDir . WORKERMAN_UPLOAD_FILENAME_PREFIX
+                . date('ymdHis') . '_' . random_int(1000, 9999);
+            file_put_contents($tmpName, $file['file_data']);
+            $_FILES[$file['name']] = [
+                'name' => $file['file_name'],
+                'type' => $file['file_type'] ?? '',
+                'tmp_name' => $tmpName,
+                'error' => UPLOAD_ERR_OK,
+                'size' => $file['file_size'],
+            ];
+        }
+        UploadedFile::reset();
+    }
+
+    /**
+     * @param string $urlPath
+     * @inheritdoc
+     */
+    public function isYii2AppUrl(string $urlPath): bool
+    {
+        foreach ($this->yii2AppUrlPrefix as $urlPrefix) {
+            if (strpos($urlPrefix, '!') === 0) {
+                $urlPrefix = substr($urlPrefix, 1);
+                if (strpos($urlPath, $urlPrefix) === 0) {
+                    return false;
+                }
+            } elseif (strpos($urlPath, $urlPrefix) === 0) {
+                return true;
+            }
+        }
+        return $this->yii2AppUrlDefault;
+    }
+
+    #endregion
+}

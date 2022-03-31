4c5649eacc9fb38779c096c13332da16d233b34c
Lujie <gao_lujie@live.cn>
2022-03-31 16:07:29 +0800

    fix

--- src/forms/AuthRoleForm.php
+++ src/forms/AuthRoleForm.php
@@ -5,6 +5,7 @@
 
 namespace lujie\auth\forms;
 
+use yii\helpers\ArrayHelper;
 use yii\rbac\Item;
 
 /**
@@ -15,4 +16,61 @@ use yii\rbac\Item;
 class AuthRoleForm extends AuthItemForm
 {
     public const TYPE = Item::TYPE_ROLE;
+
+    /**
+     * 理论上不应该Role包含Role,容易造成死循环，应该用Permission包含Permission替代，Role只包含Permission
+     * @var array
+     */
+    public $permissions;
+
+    /**
+     * @return array
+     * @inheritdoc
+     */
+    public function rules(): array
+    {
+        return array_merge($this->formRules(), [
+            [['permissions'], 'each', 'rule' => ['string']],
+        ]);
+    }
+
+    /**
+     * @param bool $insert
+     * @param array $changedAttributes
+     * @throws \yii\base\Exception
+     * @inheritdoc
+     */
+    public function afterSave($insert, $changedAttributes): void
+    {
+        $this->savePermissions();
+        parent::afterSave($insert, $changedAttributes);
+    }
+
+    /**
+     * @throws \yii\base\Exception
+     * @inheritdoc
+     */
+    public function savePermissions(): void
+    {
+        if (!is_array($this->permissions)) {
+            return;
+        }
+        $parent = $this->authManager->getRole($this->name);
+        if (empty($this->children)) {
+            $this->authManager->removeChildren($parent);
+            return;
+        }
+        $childPermissions = $this->authManager->getChildren($this->name);
+        $childPermissions = ArrayHelper::index($childPermissions, 'name');
+        foreach ($this->permissions as $permissionName) {
+            if (isset($childPermissions[$permissionName])) {
+                unset($childPermissions[$permissionName]);
+            } else if ($permission = $this->authManager->getPermission($permissionName)) {
+                $this->authManager->addChild($parent, $permission);
+            }
+        }
+        foreach ($childPermissions as $childPermission) {
+            $this->authManager->removeChild($parent, $childPermission);
+        }
+    }
 }

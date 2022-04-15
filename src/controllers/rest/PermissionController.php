c2e4f394f088319fde4cf8bd1dc845dcd2f116e7
Lujie <gao_lujie@live.cn>
2022-04-15 13:43:47 +0800

    fix

--- src/controllers/rest/PermissionController.php
+++ src/controllers/rest/PermissionController.php
@@ -6,10 +6,15 @@
 namespace lujie\auth\controllers\rest;
 
 use lujie\auth\forms\AuthPermissionForm;
+use lujie\auth\helpers\AuthHelper;
 use lujie\auth\models\AuthItem;
 use lujie\auth\searches\AuthPermissionSearch;
 use lujie\extend\rest\ActiveController;
 use Yii;
+use yii\di\Instance;
+use yii\helpers\ArrayHelper;
+use yii\rbac\DbManager;
+use yii\rbac\ManagerInterface;
 
 /**
  * Class RoleController
@@ -25,11 +30,23 @@ class PermissionController extends ActiveController
     public $searchClass = AuthPermissionSearch::class;
 
     /**
+     * @var DbManager
+     */
+    public $authManager = 'authManager';
+
+    /**
+     * @var string[]
+     */
+    public $treeChildrenKeys = ['modules', 'groups', 'permissions'];
+
+    /**
      * @return array
+     * @throws \yii\base\InvalidConfigException
      * @inheritdoc
      */
     public function actionTree(): array
     {
-        return Yii::$app->params['permissions'] ?? [];
+        $this->authManager = Instance::ensure($this->authManager, DbManager::class);
+        return AuthHelper::generatePermissionTree($this->authManager, $this->treeChildrenKeys);
     }
 }

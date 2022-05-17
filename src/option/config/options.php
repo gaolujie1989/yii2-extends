bb16b9d49b52496a92ff43507b39f34b35bd946b
Lujie <gao_lujie@live.cn>
2022-05-17 17:58:53 +0800

    fix

--- src/option/config/options.php
+++ src/option/config/options.php
@@ -4,6 +4,8 @@
  */
 
 use lujie\common\option\models\Option;
+use lujie\extend\constants\ExecStatusConst;
+use lujie\extend\constants\StatusConst;
 
 return [
     'optionType' => [
@@ -22,5 +24,17 @@ return [
         'success' => ['name' => 'Success', 'tag' => 'success' ],
         'warning' => ['name' => 'Warning', 'tag' => 'warning' ],
         'danger' => ['name' => 'Danger', 'tag' => 'danger' ],
-    ]
+    ],
+    'commonStatus' => [
+        'ACTIVE' => ['name' => 'Active', 'value' => StatusConst::STATUS_ACTIVE],
+        'INACTIVE' => ['name' => 'Inactive', 'value' => StatusConst::STATUS_INACTIVE],
+    ],
+    'execStatus' => [
+        'PENDING' => ['name' => 'Pending', 'value' => ExecStatusConst::EXEC_STATUS_PENDING],
+        'RUNNING' => ['name' => 'Running', 'value' => ExecStatusConst::EXEC_STATUS_RUNNING],
+        'SUCCESS' => ['name' => 'Success', 'value' => ExecStatusConst::EXEC_STATUS_SUCCESS],
+        'FAILED' => ['name' => 'Failed', 'value' => ExecStatusConst::EXEC_STATUS_FAILED],
+        'SKIPPED' => ['name' => 'Skipped', 'value' => ExecStatusConst::EXEC_STATUS_SKIPPED],
+        'QUEUED' => ['name' => 'Queued', 'value' => ExecStatusConst::EXEC_STATUS_QUEUED],
+    ],
 ];

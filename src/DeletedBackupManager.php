228e55618e56465a191b83cd06598f175357d697
Lujie <gao_lujie@live.cn>
2022-05-30 11:46:35 +0800

    fix

--- src/DeletedBackupManager.php
+++ src/DeletedBackupManager.php
@@ -34,12 +34,23 @@ class DeletedBackupManager extends BaseObject implements BootstrapInterface
     public $storageModelClass = DeletedData::class;
 
     /**
+     * @var bool
+     */
+    public $only = false;
+
+    /**
      * @param \yii\base\Application $app
      * @inheritdoc
      */
     public function bootstrap($app): void
     {
-        Event::on(BaseActiveRecord::class, BaseActiveRecord::EVENT_AFTER_DELETE, [$this, 'afterDeleted']);
+        if ($this->only) {
+            foreach ($this->backupModelClasses as $backupModelClass => $config) {
+                Event::on($backupModelClass, BaseActiveRecord::EVENT_AFTER_DELETE, [$this, 'afterDeleted']);
+            }
+        } else {
+            Event::on(BaseActiveRecord::class, BaseActiveRecord::EVENT_AFTER_DELETE, [$this, 'afterDeleted']);
+        }
     }
 
     /**
@@ -66,6 +77,14 @@ class DeletedBackupManager extends BaseObject implements BootstrapInterface
             'row_id' => $model->getPrimaryKey(),
             'row_data' => $model->getAttributes(),
         ]);
+        foreach ($this->backupModelClasses as $backupModelClass => $config) {
+            if ($model instanceof $backupModelClass) {
+                foreach ($config as $key => $attribute) {
+                    $storageModel->{$key} = $model->{$attribute};
+                }
+                break;
+            }
+        }
         return $storageModel->save(false);
     }
 

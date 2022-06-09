e94585767495318193fe0cded8c96568fd1ed4f3
Lujie <gao_lujie@live.cn>
2022-06-09 15:53:37 +0800

    fix

--- src/tasks/RechargeTask.php
+++ src/tasks/RechargeTask.php
@@ -7,6 +7,8 @@ namespace lujie\charging\tasks;
 
 use lujie\charging\Charger;
 use lujie\charging\models\ChargePrice;
+use lujie\executing\ProgressInterface;
+use lujie\executing\ProgressTrait;
 use lujie\scheduling\CronTask;
 use yii\di\Instance;
 
@@ -15,8 +17,10 @@ use yii\di\Instance;
  * @package lujie\charging\tasks
  * @author Lujie Zhou <gao_lujie@live.cn>
  */
-class RechargeTask extends CronTask
+class RechargeTask extends CronTask implements ProgressInterface
 {
+    use ProgressTrait;
+
     /**
      * @var Charger
      */
@@ -35,26 +39,26 @@ class RechargeTask extends CronTask
     public $createdAtFrom = '-60 days';
 
     /**
-     * @return bool
-     * @throws \Throwable
+     * @return \Generator
      * @throws \yii\base\InvalidConfigException
-     * @throws \yii\db\Exception
      * @throws \yii\db\StaleObjectException
      * @inheritdoc
      */
-    public function execute(): bool
+    public function execute(): \Generator
     {
         $this->charger = Instance::ensure($this->charger, Charger::class);
         $query = ChargePrice::find()
             ->andWhere(['>', 'created_at', strtotime($this->createdAtFrom)])
             ->andWhere($this->rechargePriceCondition);
         /** @var ChargePrice $chargePrice */
+        $progress = $this->getProgress((int)$query->count());
         foreach ($query->each() as $chargePrice) {
             $recalculate = $this->charger->recalculate($chargePrice);
             if ($recalculate === false) {
                 $chargePrice->delete();
             }
+            $progress->done++;
+            yield true;
         }
-        return true;
     }
 }

55485bbe892850bec04cae4f09d09cd8b7304dc8
Lujie <gao_lujie@live.cn>
2022-06-09 15:53:35 +0800

    fix

--- src/tasks/PullFulfillmentOrderTask.php
+++ src/tasks/PullFulfillmentOrderTask.php
@@ -5,6 +5,8 @@
 
 namespace lujie\fulfillment\tasks;
 
+use lujie\executing\ProgressInterface;
+use lujie\executing\ProgressTrait;
 use lujie\fulfillment\FulfillmentManager;
 use lujie\fulfillment\models\FulfillmentAccount;
 use lujie\scheduling\CronTask;
@@ -16,8 +18,10 @@ use yii\di\Instance;
  * @package lujie\fulfillment\tasks
  * @author Lujie Zhou <gao_lujie@live.cn>
  */
-class PullFulfillmentOrderTask extends CronTask
+class PullFulfillmentOrderTask extends CronTask implements ProgressInterface
 {
+    use ProgressTrait;
+
     /**
      * @var FulfillmentManager
      */
@@ -39,23 +43,29 @@ class PullFulfillmentOrderTask extends CronTask
     public $timeStep = 43200;
 
     /**
-     * @return bool
+     * @return \Generator
      * @throws InvalidConfigException
      * @inheritdoc
      */
-    public function execute(): bool
+    public function execute(): \Generator
     {
         $this->shippedAtFrom = is_numeric($this->shippedAtFrom) ? $this->shippedAtFrom : strtotime($this->shippedAtFrom);
         $this->shippedAtTo = is_numeric($this->shippedAtTo) ? $this->shippedAtTo : strtotime($this->shippedAtTo);
         $this->fulfillmentManager = Instance::ensure($this->fulfillmentManager, FulfillmentManager::class);
         $accountIds = FulfillmentAccount::find()->active()->column();
+        $total = ceil(($this->shippedAtTo - $this->shippedAtFrom) / $this->timeStep + 1) * count($accountIds);
+        $progress = $this->getProgress($total);
         foreach ($accountIds as $accountId) {
             for ($timeFrom = $this->shippedAtFrom; $timeFrom <= $this->shippedAtTo; $timeFrom += $this->timeStep) {
+                $progress->message = date('Y-m-d H:i', $timeFrom);
                 $timeTo = min($timeFrom + $this->timeStep, $this->shippedAtTo);
                 $this->fulfillmentManager->pullShippedFulfillmentOrders($accountId, $timeFrom, $timeTo);
+                $progress->done++;
+                yield true;
             }
             $this->fulfillmentManager->pullFulfillmentOrders($accountId);
+            $progress->done++;
+            yield true;
         }
-        return true;
     }
 }

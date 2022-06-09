827d2ba8ca48db3f8256b9f3143202144e805891
Lujie <gao_lujie@live.cn>
2022-06-09 15:53:35 +0800

    fix

--- src/tasks/PullSalesChannelOrderTask.php
+++ src/tasks/PullSalesChannelOrderTask.php
@@ -5,6 +5,8 @@
 
 namespace lujie\sales\channel\tasks;
 
+use lujie\executing\ProgressInterface;
+use lujie\executing\ProgressTrait;
 use lujie\sales\channel\models\SalesChannelAccount;
 use lujie\sales\channel\SalesChannelManager;
 use lujie\scheduling\CronTask;
@@ -16,8 +18,10 @@ use yii\di\Instance;
  * @package lujie\sales\channel\tasks
  * @author Lujie Zhou <gao_lujie@live.cn>
  */
-class PullSalesChannelOrderTask extends CronTask
+class PullSalesChannelOrderTask extends CronTask implements ProgressInterface
 {
+    use ProgressTrait;
+
     /**
      * @var SalesChannelManager
      */
@@ -39,24 +43,29 @@ class PullSalesChannelOrderTask extends CronTask
     public $timeStep = 43200;
 
     /**
-     * @return bool
+     * @return \Generator
      * @throws InvalidConfigException
      * @inheritdoc
      */
-    public function execute(): bool
+    public function execute(): \Generator
     {
         $this->createdAtFrom = is_numeric($this->createdAtFrom) ? $this->createdAtFrom : strtotime($this->createdAtFrom);
         $this->createdAtTo = is_numeric($this->createdAtTo) ? $this->createdAtTo : strtotime($this->createdAtTo);
         $this->salesChannelManager = Instance::ensure($this->salesChannelManager, SalesChannelManager::class);
         $accountIds = SalesChannelAccount::find()->active()->column();
+        $total = ceil(($this->createdAtTo - $this->createdAtFrom) / $this->timeStep + 1) * count($accountIds);
+        $progress = $this->getProgress($total);
         foreach ($accountIds as $accountId) {
             for ($timeFrom = $this->createdAtFrom; $timeFrom <= $this->createdAtTo; $timeFrom += $this->timeStep) {
+                $progress->message = date('Y-m-d H:i', $timeFrom);
                 $timeTo = min($timeFrom + $this->timeStep, $this->createdAtTo);
                 $this->salesChannelManager->pullNewSalesChannelOrders($accountId, $timeFrom, $timeTo);
+                $progress->done++;
+                yield true;
             }
-
             $this->salesChannelManager->pullSalesChannelOrders($accountId);
+            $progress->done++;
+            yield true;
         }
-        return true;
     }
 }

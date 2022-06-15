d3e628014e3590898151b9345ca18447f70704b1
Lujie <gao_lujie@live.cn>
2022-06-15 14:26:31 +0800

    init

---
+++ extension.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+return [
+    'lujie/yii2-edi' => [
+        'name' => 'lujie/yii2-edi',
+        'version' => 'dev-master',
+        'alias' => [
+            '@lujie/edi' => __DIR__ . '/src',
+            '@lujie/edi/tests' => __DIR__ . '/tests',
+        ],
+        'require' => []
+    ],
+];

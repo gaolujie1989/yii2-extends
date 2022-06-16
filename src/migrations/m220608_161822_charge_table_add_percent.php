5ef04da8cc01854449f01537bf64dd42651f24eb
Lujie <gao_lujie@live.cn>
2022-06-16 13:42:54 +0800

    fix

--- src/migrations/m220608_161822_charge_table_add_percent.php
+++ src/migrations/m220608_161822_charge_table_add_percent.php
@@ -13,4 +13,9 @@ class m220608_161822_charge_table_add_percent extends \yii\db\Migration
     {
         $this->addColumn($this->tableName, 'percent', $this->decimal(5, 2)->notNull()->defaultValue(0)->after('currency'));
     }
+
+    public function safeDown()
+    {
+        $this->dropColumn($this->tableName, 'percent');
+    }
 }

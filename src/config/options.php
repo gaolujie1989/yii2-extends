5ae7691491c181c6936810087a7783f1f5e53758
Lujie <gao_lujie@live.cn>
2022-06-17 10:52:31 +0800

    fix

---
+++ src/config/options.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+use AS2\MessageInterface;
+use AS2\PartnerInterface;
+
+return [
+    'as2MessageStatus' => [
+        'PENDING' => MessageInterface::STATUS_PENDING,
+        'SUCCESS' => MessageInterface::STATUS_SUCCESS,
+        'ERROR' => MessageInterface::STATUS_ERROR,
+        'WARNING' => MessageInterface::STATUS_WARNING,
+        'RETRY' => MessageInterface::STATUS_RETRY,
+        'IN_PROCESS' => MessageInterface::STATUS_IN_PROCESS,
+    ],
+    'as2MessageDirection' => [
+        'INBOUND' => MessageInterface::DIR_INBOUND,
+        'OUTBOUND' => MessageInterface::DIR_OUTBOUND,
+    ],
+    'as2MdnStatus' => [
+        'PENDING' => MessageInterface::MDN_STATUS_PENDING,
+        'RECEIVED' => MessageInterface::MDN_STATUS_RECEIVED,
+        'SENT' => MessageInterface::MDN_STATUS_SENT,
+        'ERROR' => MessageInterface::MDN_STATUS_ERROR,
+    ],
+    'as2MdnMode' => [
+        'SYNC' => PartnerInterface::MDN_MODE_SYNC,
+        'ASYNC' => PartnerInterface::MDN_MODE_ASYNC,
+    ],
+];

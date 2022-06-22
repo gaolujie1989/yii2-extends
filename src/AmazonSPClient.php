6030d3238d51198be61a03592c0f8a9a099fbf42
Lujie <gao_lujie@live.cn>
2022-06-22 16:19:00 +0800

    init

---
+++ src/AmazonSPClient.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\amazon\sp;
+
+use yii\base\BaseObject;
+
+/**
+ * Class AmazonSPClient
+ * @package lujie\amazon\sp
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class AmazonSPClient extends BaseObject
+{
+
+}

c7f629754b2cd8b35416554e354b5333c2b978fb
Lujie <gao_lujie@live.cn>
2022-09-08 13:14:35 +0800

    fix

--- src/tasks/PullSalesChannelOrderTask.php
+++ src/tasks/PullSalesChannelOrderTask.php
@@ -7,11 +7,13 @@ namespace lujie\sales\channel\tasks;
 
 use lujie\executing\ProgressInterface;
 use lujie\executing\ProgressTrait;
+use lujie\executing\TimeStepProgressTrait;
 use lujie\sales\channel\models\SalesChannelAccount;
 use lujie\sales\channel\SalesChannelManager;
 use lujie\scheduling\CronTask;
 use yii\base\InvalidConfigException;
 use yii\di\Instance;
+use function MongoDB\BSON\fromJSON;
 
 /**
  * Class PullSalesChannelOrderTask
@@ -20,7 +22,7 @@ use yii\di\Instance;
  */
 class PullSalesChannelOrderTask extends CronTask implements ProgressInterface
 {
-    use ProgressTrait;
+    use TimeStepProgressTrait;
 
     /**
      * @var SalesChannelManager
@@ -30,12 +32,12 @@ class PullSalesChannelOrderTask extends CronTask implements ProgressInterface
     /**
      * @var int|string
      */
-    public $createdAtFrom = '-1 days';
+    public $timeFrom = '-1 days';
 
     /**
      * @var int|string
      */
-    public $createdAtTo = 'now';
+    public $timeTo = 'now';
 
     /**
      * @var int
@@ -49,23 +51,27 @@ class PullSalesChannelOrderTask extends CronTask implements ProgressInterface
      */
     public function execute(): \Generator
     {
-        $this->createdAtFrom = is_numeric($this->createdAtFrom) ? $this->createdAtFrom : strtotime($this->createdAtFrom);
-        $this->createdAtTo = is_numeric($this->createdAtTo) ? $this->createdAtTo : strtotime($this->createdAtTo);
         $this->salesChannelManager = Instance::ensure($this->salesChannelManager, SalesChannelManager::class);
         $accountIds = SalesChannelAccount::find()->active()->column();
-        $total = ceil(($this->createdAtTo - $this->createdAtFrom) / $this->timeStep + 1) * count($accountIds);
-        $progress = $this->getProgress($total);
         foreach ($accountIds as $accountId) {
-            for ($timeFrom = $this->createdAtFrom; $timeFrom <= $this->createdAtTo; $timeFrom += $this->timeStep) {
-                $progress->message = date('Y-m-d H:i', $timeFrom);
-                $timeTo = min($timeFrom + $this->timeStep, $this->createdAtTo);
-                $this->salesChannelManager->pullNewSalesChannelOrders($accountId, $timeFrom, $timeTo);
-                $progress->done++;
-                yield true;
-            }
+            yield from $this->executeProgress([$accountId], count($accountIds));
             $this->salesChannelManager->pullSalesChannelOrders($accountId);
-            $progress->done++;
-            yield true;
+            yield $accountId;
         }
+        return true;
+    }
+
+    /**
+     * @param int $timeAtFrom
+     * @param int $timeAtTo
+     * @param array $params
+     * @return mixed
+     * @throws InvalidConfigException
+     * @inheritdoc
+     */
+    protected function executeTimeStep(int $timeAtFrom, int $timeAtTo, array $params = []): mixed
+    {
+        [$accountId] = $params;
+        $this->salesChannelManager->pullNewSalesChannelOrders($accountId, $timeAtFrom, $timeAtTo);
     }
 }

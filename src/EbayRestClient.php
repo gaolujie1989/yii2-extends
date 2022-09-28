6f717e7e23e7fa266e69ade182744e077086a7a2
Lujie <gao_lujie@live.cn>
2022-09-28 15:36:32 +0800

    fix

--- src/EbayRestClient.php
+++ src/EbayRestClient.php
@@ -8,7 +8,6 @@ namespace lujie\ebay;
 use lujie\extend\authclient\OAuth2ExtendTrait;
 use lujie\extend\authclient\RestApiTrait;
 use yii\authclient\OAuth2;
-use yii\authclient\OAuthToken;
 use yii\httpclient\Client;
 
 /**
@@ -44,8 +43,6 @@ class EbayRestClient extends OAuth2
 
     public $scope = [
         'https://api.ebay.com/oauth/api_scope',
-        'https://api.ebay.com/oauth/api_scope/buy.order.readonly',
-        'https://api.ebay.com/oauth/api_scope/buy.guest.order',
         'https://api.ebay.com/oauth/api_scope/sell.marketing.readonly',
         'https://api.ebay.com/oauth/api_scope/sell.marketing',
         'https://api.ebay.com/oauth/api_scope/sell.inventory.readonly',
@@ -55,22 +52,9 @@ class EbayRestClient extends OAuth2
         'https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly',
         'https://api.ebay.com/oauth/api_scope/sell.fulfillment',
         'https://api.ebay.com/oauth/api_scope/sell.analytics.readonly',
-        'https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly',
-        'https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly',
-        'https://api.ebay.com/oauth/api_scope/buy.shopping.cart',
-        'https://api.ebay.com/oauth/api_scope/buy.offer.auction',
-        'https://api.ebay.com/oauth/api_scope/commerce.identity.readonly',
-        'https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly',
-        'https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly',
-        'https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly',
-        'https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly',
-        'https://api.ebay.com/oauth/api_scope/commerce.identity.status.readonly',
         'https://api.ebay.com/oauth/api_scope/sell.finances',
-        'https://api.ebay.com/oauth/api_scope/sell.item.draft',
         'https://api.ebay.com/oauth/api_scope/sell.payment.dispute',
-        'https://api.ebay.com/oauth/api_scope/sell.item',
-        'https://api.ebay.com/oauth/api_scope/sell.reputation',
-        'https://api.ebay.com/oauth/api_scope/sell.reputation.readonly',
+        'https://api.ebay.com/oauth/api_scope/commerce.identity.readonly',
         'https://api.ebay.com/oauth/api_scope/commerce.notification.subscription',
         'https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly',
     ];

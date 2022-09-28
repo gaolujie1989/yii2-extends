0ee4d33f4da3a81e5f13c6d4e641a434d6695ce7
Lujie <gao_lujie@live.cn>
2022-09-28 14:04:19 +0800

    add ebay rest

---
+++ src/EbayRestClient.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\ebay;
+
+use lujie\extend\authclient\RestApiTrait;
+use yii\authclient\OAuth2;
+use yii\authclient\OAuthToken;
+use yii\httpclient\Client;
+
+/**
+ * Class EbayRestClient
+ *
+ * @method array listOrders($data = [])
+ * @method \Generator eachOrder($condition = [], $batchSize = 100)
+ * @method \Generator batchOrder($condition = [], $batchSize = 100)
+ * @method array getOrder($data)
+ * @method array createOrder($data)
+ * @method array updateOrder($data)
+ * @method array deleteOrder($data)
+ *
+ * @package lujie\ebay
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class EbayRestClient extends OAuth2
+{
+    use RestApiTrait;
+
+    protected $sandbox = false;
+
+    public $sandboxUrlMap = [
+        'auth.ebay.com' => 'auth.sandbox.ebay.com',
+        'api.ebay.com' => 'api.sandbox.ebay.com',
+    ];
+
+    public $apiBaseUrl = 'https://api.ebay.com/';
+
+    public $authUrl = 'https://auth.ebay.com/oauth2/authorize';
+
+    public $tokenUrl = 'https://api.ebay.com/identity/v1/oauth2/token';
+
+    public $scope = [
+        'https://api.ebay.com/oauth/api_scope',
+        'https://api.ebay.com/oauth/api_scope/buy.order.readonly',
+        'https://api.ebay.com/oauth/api_scope/buy.guest.order',
+        'https://api.ebay.com/oauth/api_scope/sell.marketing.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.marketing',
+        'https://api.ebay.com/oauth/api_scope/sell.inventory.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.inventory',
+        'https://api.ebay.com/oauth/api_scope/sell.account.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.account',
+        'https://api.ebay.com/oauth/api_scope/sell.fulfillment.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.fulfillment',
+        'https://api.ebay.com/oauth/api_scope/sell.analytics.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.marketplace.insights.readonly',
+        'https://api.ebay.com/oauth/api_scope/commerce.catalog.readonly',
+        'https://api.ebay.com/oauth/api_scope/buy.shopping.cart',
+        'https://api.ebay.com/oauth/api_scope/buy.offer.auction',
+        'https://api.ebay.com/oauth/api_scope/commerce.identity.readonly',
+        'https://api.ebay.com/oauth/api_scope/commerce.identity.email.readonly',
+        'https://api.ebay.com/oauth/api_scope/commerce.identity.phone.readonly',
+        'https://api.ebay.com/oauth/api_scope/commerce.identity.address.readonly',
+        'https://api.ebay.com/oauth/api_scope/commerce.identity.name.readonly',
+        'https://api.ebay.com/oauth/api_scope/commerce.identity.status.readonly',
+        'https://api.ebay.com/oauth/api_scope/sell.finances',
+        'https://api.ebay.com/oauth/api_scope/sell.item.draft',
+        'https://api.ebay.com/oauth/api_scope/sell.payment.dispute',
+        'https://api.ebay.com/oauth/api_scope/sell.item',
+        'https://api.ebay.com/oauth/api_scope/sell.reputation',
+        'https://api.ebay.com/oauth/api_scope/sell.reputation.readonly',
+        'https://api.ebay.com/oauth/api_scope/commerce.notification.subscription',
+        'https://api.ebay.com/oauth/api_scope/commerce.notification.subscription.readonly',
+    ];
+
+    /**
+     * @var array
+     */
+    public $resources = [
+        'Order' => 'sell/fulfillment/v1/order',
+    ];
+
+    /**
+     * @var string
+     */
+    public $version = 'v1';
+
+    /**
+     * @var array
+     */
+    public $extraActions = [
+    ];
+
+    /**
+     * @inheritdoc
+     */
+    public function init(): void
+    {
+        parent::init();
+        $this->initRest();
+        if (is_array($this->scope)) {
+            $this->scope = implode(' ', $this->scope);
+        }
+    }
+
+    /**
+     * @param bool $sandbox
+     * @inheritdoc
+     */
+    public function setSandbox(bool $sandbox = true): void
+    {
+        $this->sandbox = $sandbox;
+        $map = $this->sandbox ? $this->sandboxUrlMap : array_flip($this->sandboxUrlMap);
+        $this->apiBaseUrl = strtr($this->apiBaseUrl, $map);
+        $this->authUrl = strtr($this->authUrl, $map);
+        $this->tokenUrl = strtr($this->tokenUrl, $map);
+    }
+
+    protected function initUserAttributes()
+    {
+    }
+
+    /**
+     * @param array $tokenConfig
+     * @return OAuthToken
+     * @inheritdoc
+     */
+    protected function createToken(array $tokenConfig = []): OAuthToken
+    {
+        $tokenConfig['tokenSecretParamKey'] = 'refresh_token';
+        $authToken = parent::createToken($tokenConfig);
+        //To Avoid 401
+        $authToken->setExpireDuration($authToken->getExpireDuration() - 5);
+        return $authToken;
+    }
+
+    /**
+     * @param \yii\httpclient\Request $request
+     * @inheritdoc
+     */
+    protected function applyClientCredentialsToRequest($request): void
+    {
+        $request->getHeaders()->set('Authorization', 'Basic ' . base64_encode($this->clientId . ':' . $this->clientSecret));
+        if ($request->getUrl() === $this->tokenUrl) {
+            $request->format = Client::FORMAT_URLENCODED;
+        }
+    }
+
+    /**
+     * @param \yii\httpclient\Request $request
+     * @param OAuthToken $accessToken
+     * @inheritdoc
+     */
+    public function applyAccessTokenToRequest($request, $accessToken): void
+    {
+        $request->getHeaders()->set('Authorization', 'Bearer ' . $accessToken->getToken());
+    }
+}

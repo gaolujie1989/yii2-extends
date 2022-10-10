1b927e64d4cc27f75114fedafe2b2fe71480da57
Lujie <gao_lujie@live.cn>
2022-10-10 13:55:20 +0800

    fix

---
+++ src/f4px/F4pxFulfillmentService.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\fulfillment\f4px;
+
+/**
+ * Class F4pxFulfillmentService
+ * @package lujie\fulfillment\f4px
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ * @deprecated
+ */
+class F4pxFulfillmentService extends \lujie\fulfillment\services\f4px\F4pxFulfillmentService
+{
+}
---
+++ src/pm/PmFulfillmentServiceLoader.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * @copyright Copyright (c) 2019
+ */
+
+namespace lujie\fulfillment\pm;
+
+/**
+ * Class PmFulfillmentService
+ * @package lujie\fulfillment\pm
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class PmFulfillmentServiceLoader extends \lujie\fulfillment\services\pm\PmFulfillmentServiceLoader
+{
+}

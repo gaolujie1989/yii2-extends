47d46b4abac9ec259fa546ac607ae74891bec11e
Lujie <gao_lujie@live.cn>
2023-02-01 17:30:56 +0800

    fix

--- src/searches/SalesChannelItemSearch.php
+++ src/searches/SalesChannelItemSearch.php
@@ -6,14 +6,14 @@
 namespace lujie\sales\channel\searches;
 
 use lujie\extend\db\SearchTrait;
-use lujie\sales\channel\models\SalesChannelOrder;
+use lujie\sales\channel\models\SalesChannelItem;
 
 /**
  * Class SalesChannelOrderSearch
  * @package lujie\sales\channel\searches
  * @author Lujie Zhou <gao_lujie@live.cn>
  */
-class SalesChannelItemSearch extends SalesChannelOrder
+class SalesChannelItemSearch extends SalesChannelItem
 {
     use SearchTrait;
 }

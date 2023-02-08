c6259ec8064082eb04b25d5618c7948402074311
Lujie <gao_lujie@live.cn>
2023-02-08 17:50:43 +0800

    fix

--- src/helpers/ExecuteException.php
+++ src/helpers/ExecuteException.php
@@ -5,6 +5,8 @@
 
 namespace lujie\extend\helpers;
 
+use lujie\extend\constants\ExecStatusConst;
+use Throwable;
 use yii\base\Exception;
 
 /**
@@ -17,4 +19,20 @@ class ExecuteException extends Exception
     public $status;
 
     public $result = [];
+
+    public function __construct($message = "", $status = ExecStatusConst::EXEC_STATUS_REMOTE_PROCESSING, $result = [], $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->status = $status;
+        $this->result = $result;
+    }
+
+    /**
+     * @return string
+     * @inheritdoc
+     */
+    public function getName(): string
+    {
+        return 'Execute Exception';
+    }
 }

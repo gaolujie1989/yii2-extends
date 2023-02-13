93211f2304fefc6b634c2caa21f03115d40e5c0a
Lujie <gao_lujie@live.cn>
2023-02-13 16:15:46 +0800

    fix

--- src/option/providers/OttoCategoryProvider.php
+++ src/option/providers/OttoCategoryProvider.php
@@ -3,24 +3,47 @@
  * @copyright Copyright (c) 2019
  */
 
-use lujie\common\option\providers\QueryOptionProvider;
+namespace lujie\sales\channel\option\providers;
+
+use lujie\common\option\providers\DbOptionProvider;
+use lujie\sales\channel\models\OttoCategory;
+use yii\db\QueryInterface;
 
 /**
  * Class OttoCategoryProvider
  * @author Lujie Zhou <gao_lujie@live.cn>
  */
-class OttoCategoryProvider extends QueryOptionProvider
+class OttoCategoryProvider extends DbOptionProvider
 {
-    public $type = 'ottoCategory';
+    public $modelClass = OttoCategory::class;
+
+    public $filterKeys = ['category_group', 'name'];
+
+    public $separator = '>>';
+
+    public function getOptions(string $type, ?string $key = null): array
+    {
+        $ottoCategories = parent::getOptions($type, $key);
+        return array_map(function(array $category) {
+            $categoryPath = $category['category_group'] . $this->separator . $category['name'];
+            return [
+                'value' => $categoryPath,
+                'label' => $categoryPath,
+            ];
+        }, $ottoCategories);
+    }
 
     /**
      * @param string $type
      * @param string|null $key
-     * @return array
+     * @return QueryInterface
+     * @throws \yii\base\InvalidConfigException
      * @inheritdoc
      */
-    public function getOptions(string $type, ?string $key = null): array
+    protected function getQuery(string $type, ?string $key = null): QueryInterface
     {
-
+        $query = parent::getQuery($type, $key);
+        $query->addOrderBy(['category_group' => SORT_ASC, 'name' => SORT_ASC]);
+        return $query;
     }
 }

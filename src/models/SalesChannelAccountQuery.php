8487d80609a3f0e0eb348665357512a285e21823
Lujie <gao_lujie@live.cn>
2023-02-16 14:38:26 +0800

    fix

--- src/models/SalesChannelAccountQuery.php
+++ src/models/SalesChannelAccountQuery.php
@@ -12,6 +12,7 @@ use lujie\common\account\models\AccountQuery;
  *
  * @method SalesChannelAccountQuery id($id)
  * @method SalesChannelAccountQuery orderById($sort = SORT_ASC)
+ * @method SalesChannelAccountQuery indexById()
  * @method int getId()
  * @method array getIds()
  *
@@ -20,9 +21,21 @@ use lujie\common\account\models\AccountQuery;
  * @method SalesChannelAccountQuery type($type)
  * @method SalesChannelAccountQuery status($status)
  * @method SalesChannelAccountQuery name($name)
+ * @method SalesChannelAccountQuery username($username)
+ *
+ * @method SalesChannelAccountQuery createdAtBetween($from, $to = null)
+ * @method SalesChannelAccountQuery updatedAtBetween($from, $to = null)
  *
  * @method SalesChannelAccountQuery active()
  * @method SalesChannelAccountQuery inActive()
+ *
+ * @method SalesChannelAccountQuery orderByAccountId($sort = SORT_ASC)
+ * @method SalesChannelAccountQuery orderByCreatedAt($sort = SORT_ASC)
+ * @method SalesChannelAccountQuery orderByUpdatedAt($sort = SORT_ASC)
+ *
+ * @method SalesChannelAccountQuery indexByAccountId()
+ *
+ * @method array getAccountIds()
  *
  * @method array|SalesChannelAccount[] all($db = null)
  * @method array|SalesChannelAccount|null one($db = null)

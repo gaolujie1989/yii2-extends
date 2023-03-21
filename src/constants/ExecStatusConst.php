c74889e38be577c52bf947c723feda51c98c36d5
Lujie <gao_lujie@live.cn>
2023-03-21 14:21:15 +0800

    fix

--- src/constants/ExecStatusConst.php
+++ src/constants/ExecStatusConst.php
@@ -14,7 +14,8 @@ class ExecStatusConst
 {
     public const EXEC_STATUS_PENDING = 0; //wait to run
     public const EXEC_STATUS_RUNNING = 1;
-    public const EXEC_STATUS_INVALID = 5; //failed by validation
+    public const EXEC_STATUS_INVALID = 5; //failed by validation, should not retry
+    public const EXEC_STATUS_EXTERNAL_PROCESSING = 9; //processing by external service, should queue(but not set status to queued) and wait for callback
     public const EXEC_STATUS_SUCCESS = 10;
     public const EXEC_STATUS_FAILED = 11; //failed by code exception, need retry(network) or fix, should throw exception and notify to developer
     public const EXEC_STATUS_SKIPPED = 12; //not execute by lock or data validation failed

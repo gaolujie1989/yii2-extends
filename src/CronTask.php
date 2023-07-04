dd05acf62c502072e57ce371191288842131afcb
Lujie <gao_lujie@live.cn>
2023-07-04 11:54:55 +0800

    fix

--- src/CronTask.php
+++ src/CronTask.php
@@ -18,4 +18,19 @@ use yii\base\Model;
 class CronTask extends Model implements ScheduleTaskInterface
 {
     use CronScheduleTrait, ExecutableTrait, LockableTrait, QueueableTrait;
+
+    /**
+     * @return int
+     * @inheritdoc
+     */
+    public function getTtr(): int
+    {
+        if ($this->ttr > 0) {
+            return $this->ttr;
+        }
+        if (str_starts_with($this->expression, '*/')) {
+            return (int)substr($this->expression, 2) * 60;
+        }
+        return $this->ttr;
+    }
 }

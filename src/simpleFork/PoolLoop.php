376675a3e501f3a006b5193a5a4260d3cba020fe
Lujie <gao_lujie@live.cn>
2023-07-27 14:28:22 +0800

    fix

---
+++ src/simpleFork/PoolLoop.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright Copyright (c) 2023
+ */
+
+namespace lujie\extend\simpleFork;
+
+use Jenner\SimpleFork\AbstractPool;
+
+/**
+ * Class PoolLoop
+ * @package lujie\extend\simpleFork
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class PoolLoop implements PoolLoopInterface
+{
+    /**
+     * @var callable
+     */
+    public $callable;
+
+    /**
+     * @param AbstractPool $pool
+     * @inheritdoc
+     */
+    public function loop(AbstractPool $pool): void
+    {
+        call_user_func($this->callable, $pool);
+    }
+}

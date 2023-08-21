ef850082354483395e6ba5794b37014e0dce4ce9
Lujie <gao_lujie@live.cn>
2023-08-21 17:26:50 +0800

    fix

--- src/BaseAmazonSPClient.php
+++ src/BaseAmazonSPClient.php
@@ -6,6 +6,7 @@
 namespace lujie\amazon\sp;
 
 use DoubleBreak\Spapi\Client;
+use DoubleBreak\Spapi\Credentials;
 use lujie\extend\authclient\BatchApiTrait;
 
 /**
@@ -18,7 +19,53 @@ class BaseAmazonSPClient extends Client
     use BatchApiTrait;
 
     /**
-     * @param string $apiSubUrl
+     * @var Credentials
+     */
+    protected $credentialsInstance;
+
+    /**
+     * @var array[]
+     */
+    public $rdtOperations = [
+        '/vendor/directFulfillment/orders/v1/purchaseOrders' => [
+            'dataElements' => ['shipToParty']
+        ],
+        '/vendor/directFulfillment/orders/2021-12-28/purchaseOrders' => [
+            'dataElements' => ['shipToParty']
+        ],
+        '/vendor/directFulfillment/shipping/v1/shippingLabels' => [
+            'dataElements' => ['labelData']
+        ],
+        '/vendor/directFulfillment/shipping/v1/packingSlips' => [
+            'dataElements' => ['content']
+        ],
+        '/vendor/directFulfillment/shipping/v1/customerInvoices' => [
+            'dataElements' => ['content']
+        ],
+        '/vendor/directFulfillment/shipping/2021-12-28/shippingLabels' => [
+            'dataElements' => ['labelData']
+        ],
+        '/vendor/directFulfillment/shipping/2021-12-28/packingSlips' => [
+            'dataElements' => ['content']
+        ],
+        '/vendor/directFulfillment/shipping/2021-12-28/customerInvoices' => [
+            'dataElements' => ['content']
+        ],
+    ];
+
+    /**
+     * @param Credentials $credentials
+     * @param array $config
+     * @throws \Exception
+     */
+    public function __construct(Credentials $credentials, array $config = [])
+    {
+        $this->credentialsInstance = $credentials;
+        parent::__construct($credentials->getCredentials(), $config);
+    }
+
+    /**
+     * @param string|array $apiSubUrl
      * @param string $method
      * @param array|string $data
      * @param array $headers
@@ -38,6 +85,12 @@ class BaseAmazonSPClient extends Client
         }
         if ($data) {
             $requestOptions['json'] = $data;
+        }
+
+        if ($restrictedResource = $this->getRestrictedResource($uri, $method)) {
+            $this->credentials = $this->credentialsInstance->getRdtCredentials($restrictedResource);
+        } else {
+            $this->credentials = $this->credentialsInstance->getCredentials();
         }
         return $this->send($uri, $requestOptions);
     }
@@ -64,5 +117,22 @@ class BaseAmazonSPClient extends Client
         $payload = $responseData['payload'] ?? [];
         unset($payload['pagination']);
         return $payload ? reset($payload) : [];
+    }
+
+    /**
+     * @param string $uriPath
+     * @return array|null
+     * @inheritdoc
+     */
+    protected function getRestrictedResource(string $uriPath, string $method): ?array
+    {
+        foreach ($this->rdtOperations as $urlPrefix => $restrictedResource) {
+            if (str_starts_with($urlPrefix, $urlPrefix)) {
+                $restrictedResource['method'] = $method;
+                $restrictedResource['path'] = $uriPath;
+                return $restrictedResource;
+            }
+        }
+        return null;
     }
 }

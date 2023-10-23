75e620b15983bd9639a493deec360912da39a51f
Lujie <gao_lujie@live.cn>
2023-10-23 13:56:32 +0800

    fix

---
+++ src/history/models/ModelHistoryRelationTrait.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @copyright Copyright (c) 2023
+ */
+
+namespace lujie\common\history\models;
+
+use yii\db\ActiveQuery;
+
+/**
+ * Trait ModelHistoryRelationTrait
+ *
+ * @property string $historyClass
+ *
+ * @package lujie\common\history\models
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+trait ModelHistoryRelationTrait
+{
+    /**
+     * @return ActiveQuery
+     * @inheritdoc
+     */
+    public function getHistories(): ActiveQuery
+    {
+        $modelPk = self::primaryKey()[0];
+        $historyClass = $this->historyClass ?? ModelHistory::class;
+        $historyPk = $historyClass::primaryKey()[0];
+        return $this->hasMany($historyClass, ['model_id' => $modelPk])
+            ->with(['details'])
+            ->addOrderBy([$historyPk => SORT_DESC])
+            ->limit(100);
+    }
+}

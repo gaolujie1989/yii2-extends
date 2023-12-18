b20555ac1586dc1ca9c716265a77cb86c35f202b
Lujie <gao_lujie@live.cn>
2023-12-18 14:49:15 +0800

    fix

--- src/calculators/ShippingItem.php
+++ src/calculators/ShippingItem.php
@@ -35,7 +35,7 @@ class ShippingItem extends BaseChargeItem
     /**
      * @var int
      */
-    public $shippedAt = 0;
+    public $shippedAt;
 
     /**
      * @var int
@@ -56,4 +56,42 @@ class ShippingItem extends BaseChargeItem
      * @var int
      */
     public $heightMM;
+
+    /**
+     * @param int $weightG
+     * @param int $lengthMM
+     * @param int $widthMM
+     * @param int $heightMM
+     * @param string $carrier
+     * @param string $postalCode
+     * @param string $destination
+     * @param string $departure
+     * @param int|null $shippedAt
+     * @return static
+     * @inheritdoc
+     */
+    public static function create(
+        int $weightG,
+        int $lengthMM,
+        int $widthMM,
+        int $heightMM,
+        string $carrier,
+        string $postalCode,
+        string $destination,
+        string $departure,
+        int $shippedAt = null,
+    ): static
+    {
+        $shippingItem = new ShippingItem();
+        $shippingItem->weightG = $weightG;
+        $shippingItem->lengthMM = $lengthMM;
+        $shippingItem->widthMM = $widthMM;
+        $shippingItem->heightMM = $heightMM;
+        $shippingItem->carrier = $carrier;
+        $shippingItem->postalCode = $postalCode;
+        $shippingItem->destination = $destination;
+        $shippingItem->departure = $departure;
+        $shippingItem->shippedAt = $shippedAt ?: time();
+        return $shippingItem;
+    }
 }

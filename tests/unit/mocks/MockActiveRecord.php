59e53eb2d6deac13f8eb7eba5e32826529cf231b
Lujie <gao_lujie@live.cn>
2024-01-04 14:24:27 +0800

    fix

--- tests/unit/mocks/MockActiveRecord.php
+++ tests/unit/mocks/MockActiveRecord.php
@@ -13,6 +13,7 @@ use lujie\extend\db\ModelFileDeleteTrait;
 use lujie\extend\db\SaveTrait;
 use lujie\extend\db\TraceableBehaviorTrait;
 use lujie\extend\db\TransactionTrait;
+use lujie\extend\file\Base64ContentTrait;
 use yii\db\ActiveRecord;
 
 /**
@@ -24,7 +25,7 @@ class MockActiveRecord extends ActiveRecord
 {
     use TraceableBehaviorTrait, AliasBehaviorTrait,
         AliasFieldTrait, SaveTrait, DeleteTrait, ModelFileDeleteTrait,
-        TransactionTrait, DbConnectionTrait;
+        TransactionTrait, DbConnectionTrait, Base64ContentTrait;
 
     public static $inserts = [];
 

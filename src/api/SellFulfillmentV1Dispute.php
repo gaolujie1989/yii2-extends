b3bfdfb5474c5c763e6f4ccbbda349f28a9f3d16
Lujie <gao_lujie@live.cn>
2024-04-03 17:48:03 +0800

    fix

---
+++ src/api/SellFulfillmentV1Dispute.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace lujie\ebay\api;
+
+/**
+ * Class SellFulfillmentV1Dispute
+ * @package lujie\ebay\api
+ * @author Lujie Zhou <gao_lujie@live.cn>
+ */
+class SellFulfillmentV1Dispute extends SellFulfillmentV1
+{
+    public $apiBaseUrl = 'https://apiz.ebay.com/sell/fulfillment/v1';
+}

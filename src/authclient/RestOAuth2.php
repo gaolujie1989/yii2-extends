3da0e00e74c815b1cc6e65ee2897b331aaafebd7
Lujie <gao_lujie@live.cn>
2024-07-25 15:33:03 +0800

    fix

--- src/authclient/RestOAuth2.php
+++ src/authclient/RestOAuth2.php
@@ -15,4 +15,6 @@ use yii\authclient\OAuth2;
 class RestOAuth2 extends OAuth2
 {
     use RestApiTrait, BatchApiTrait, OAuthExtendTrait, SandboxApiTrait;
+
+    public $accessTokenLocation = self::ACCESS_TOKEN_LOCATION_HEADER;
 }
